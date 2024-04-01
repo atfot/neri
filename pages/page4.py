6d5df8001359cc6c4476c23e909425dc84306819
atfot <104991246+atfot@users.noreply.github.com>
2024-04-01 19:46:07 +0900

    afsdfd

--- pages/page4.py
+++ pages/page4.py
@@ -108,6 +108,7 @@ if prompt := st.chat_input():
         - Keep your responses between one and three sentences.
         - The best response should be sentences covered with quotes.
         - Never reuse answers that have already been used within a conversation.]
+        - Use only one period for the most important sentences, and between two and three periods for the rest. End sentences with "." for the most important sentences and "..." or "..." for the rest.
 
         **Three possible answers from a psychotherapist**: 
         [Given the above summary and the conversation, what are three possible answers a psychotherapist might give here?]
@@ -121,6 +122,7 @@ if prompt := st.chat_input():
         - You must write down "**THINGS YOU NEED TO REMEMBER BEFORE THE ANSWER**" into the form.
         - If you get a short answer from the mental patient, you must ask him/her a related question.
         - Never reuse answers that have already been used within a conversation.
+        - Use only one period for the most important sentences, and between two and three periods for the rest. End sentences with "." for the most important sentences and "..." or "..." for the rest.
     """
     my_bar.progress(20,text=progress_text)
     response = client.chat.completions.create(
@@ -135,9 +137,9 @@ if prompt := st.chat_input():
       "content": f"{user_prompt_1}"
     }
   ],
-  temperature=1,
+  temperature=0.1,
   max_tokens=512,
-  top_p=0.1,
+  top_p=1,
   frequency_penalty=1,
   presence_penalty=1
 )
