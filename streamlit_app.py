77085c2fdf35cda2130107eb0b1c6fc21cc422c3
atfot <104991246+atfot@users.noreply.github.com>
2024-04-08 19:20:41 +0900

    aㅁㄴㅇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,9 @@ if language_selection:
     st.title('')
     col1,col2=st.columns([5,5])
     with col2:
-        st.write("Please login (username `test`, password `test`).")
+        st.write("""Please login 
+                 
+                 (username `test`, password `test`).""")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
@@ -70,7 +72,9 @@ if not language_selection:
     st.title('')
     col1,col2=st.columns([4.7,5.3])
     with col2:
-        st.write("로그인해주세요 (유저이름 `test`, 패스워드 `test`).")
+        st.write("""로그인해주세요 
+                 
+                 (유저이름 `test`, 패스워드 `test`).""")
     username = st.text_input("유저 이름")
     password = st.text_input("비밀번호", type="password")
 
