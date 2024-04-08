5210fe7c462ad12fe4b9c783bebd5561aa548d06
atfot <104991246+atfot@users.noreply.github.com>
2024-04-08 22:40:14 +0900

    asdf

--- navigation.py
+++ navigation.py
@@ -45,6 +45,8 @@ def logout():
         del st.session_state["messages"]
         del st.session_state['conversations']
         del st.session_state['message_summary']
+    else:
+        pass
     st.info("Logged out successfully!")
     sleep(0.5)
     st.switch_page("streamlit_app.py")
