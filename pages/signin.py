048bc34b95c8b65ff97eaf079829f3f84afcd4b4
atfot <104991246+atfot@users.noreply.github.com>
2024-04-15 18:28:11 +0900

    asdf

--- pages/signin.py
+++ pages/signin.py
@@ -85,7 +85,7 @@ if st.session_state.korean_mode==1:
                     time.sleep(1)
                     my_bar.empty()
                 finally:
-                    st.switch_page('pages/page4')
+                    st.switch_page('pages/page4.py')
             else:
                 pass
 if st.session_state.korean_mode==0:
@@ -168,6 +168,6 @@ if st.session_state.korean_mode==0:
                     time.sleep(1)
                     my_bar.empty()
                 finally:
-                    st.switch_page('pages/page2')
+                    st.switch_page("pages/page2.py")
             else:
                 pass
