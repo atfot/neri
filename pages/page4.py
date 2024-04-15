6ceef5de4d250c5cfc9adb52b87c21ffa8fd0bc2
atfot <104991246+atfot@users.noreply.github.com>
2024-04-15 21:17:56 +0900

    asd

--- pages/page4.py
+++ pages/page4.py
@@ -10,6 +10,14 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+if 'username' not in st.session_state:
+   st.session_state.username=st.secrets.username
+   st.session_state.age=st.secrets.age
+   st.session_state.gender=st.secrets.user_gender
+   st.session_state.gender=st.secrets.user_gender
+   st.session_state.problem=st.secrets.problem
+   st.session_state.problem_explanation=st.secrets.problem_explanation
+
 if 'client' not in st.session_state:
   st.session_state.client = OpenAI(api_key=st.secrets['api_key'])
 
