a870f54157d4aa79f2662d83d4fcb8fc0bf2138f
atfot <104991246+atfot@users.noreply.github.com>
2024-04-15 20:15:18 +0900

    asdf

--- pages/signin.py
+++ pages/signin.py
@@ -101,10 +101,7 @@ if st.session_state.korean_mode==1:
                     df,
                     use_container_width=True,
                     column_order=None,
-                    column_config={
-                        ''
-                    }
-                    )
+                    column_config=None
                 st.markdown(df.style.set_properties(**{'color': '#FF0000','font-weight':'bold'}.hide(axis = 0).hide(axis = 1).to_html(), unsafe_allow_html = True))
                 time.sleep(5)
                 try:
