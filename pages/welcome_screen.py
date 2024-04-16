395ee16e363dc7b1b8c9f82c32ba6c5b4b7957e0
atfot <104991246+atfot@users.noreply.github.com>
2024-04-16 13:09:34 +0900

    asdf

--- pages/welcome_screen.py
+++ pages/welcome_screen.py
@@ -1,1 +1,13 @@
 import streamlit as st
+
+st.title('Welcome to Neri!')
+st.title('')
+st.write("""
+Neri is an **AI Therapist Chatbot**, using **OpenAI's ChatGPT API**.
+         
+You can choose either GPT 3.5 or GPT 4.0 depending on your own needs.
+
+
+         
+
+""")
