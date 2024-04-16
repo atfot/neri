16dc2363fe92731ba7cdd1121a970894d743117a
atfot <104991246+atfot@users.noreply.github.com>
2024-04-16 13:46:08 +0900

    asdf

--- pages/korean_chatbot.py
+++ pages/korean_chatbot.py
@@ -299,4 +299,8 @@ if prompt := st.chat_input():
     with col2:
        st.write('')
        if st.button('다른 대답'):
-          st.write(st.session_state.messages[:-1])
+        st.session_state.test=True
+    if session_state.test:
+      st.write(st.session_state.messages[:-1])
+    else:
+       pass
