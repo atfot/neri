f805b36e606d00af9b8a7deb3915ad743060842e
atfot <104991246+atfot@users.noreply.github.com>
2024-04-18 18:24:49 +0900

    asdf

--- pages/test.py
+++ pages/test.py
@@ -4,6 +4,12 @@ from korean_navigation import make_sidebar
 
 make_sidebar()
 
+if "messages" in st.session_state:
+    del st.session_state["messages"]
+    del st.session_state['conversations']
+    del st.session_state['message_summary']
+    del st.session_state.client
+
 # variables
 if "messages" not in st.session_state:
     st.session_state.messages = []
