3391e16e99e6f984a98ac79aa042cba59d5606c8
atfot <104991246+atfot@users.noreply.github.com>
2024-04-18 15:24:42 +0900

    asd

--- pages/test.py
+++ pages/test.py
@@ -30,10 +30,12 @@ def main():
         # Get the last user prompt in the msg history.
         if st.session_state.repeat:
             if len(st.session_state.messages)<=2:
-                prompt = st.session_state.messages[-1]['content']
+                st.write(len(st.session_state.messages))
+                prompt = st.session_state.messages[-2]['content']
                 st.session_state.messages=[]
                 st.session_state.repeat = False  # reset
             if len(st.session_state.messages)>=2:
+                st.write(len(st.session_state.messages))
                 prompt = st.session_state.messages[-2]['content']
                 st.session_state.messages=st.session_state.messages[:-2]
                 st.session_state.repeat = False  # reset
