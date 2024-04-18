623cca428b3293397177c3a95a4c5e63d534cf2a
atfot <104991246+atfot@users.noreply.github.com>
2024-04-18 19:13:21 +0900

    ㅁㄴㅇㄹ

--- pages/test.py
+++ pages/test.py
@@ -63,7 +63,7 @@ def main():
         # Get the last user prompt in the msg history.
         if st.session_state.repeat:
             prompt = st.session_state.msg[-2]['content']
-            st.session_state.msg.append({"role": "user", "content": prompt})
+            #st.session_state.msg.append({"role": "user", "content": prompt})
             st.session_state.repeat = False  # reset
         else:
             # Only print the user msg if repeat is false.
