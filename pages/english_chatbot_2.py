ecb5468d8ad1e333722120ae2b62b2ea83c4cb4c
atfot <104991246+atfot@users.noreply.github.com>
2024-04-18 21:56:10 +0900

    asdf

--- pages/english_chatbot_2.py
+++ pages/english_chatbot_2.py
@@ -213,7 +213,7 @@ def main():
                 
                 **Conversation content**: [{st.session_state.conversations}]
 
-                **Three possible answers from a psychotherapist who wants to know and learn about his patient**: 
+                **Three possible answers from a psychotherapist who wants to learn about his patient and heal the patient's mind**: 
                 "[{msg}]"
 
                 - After reading the informations above, please **pick the best response from three possible answers** and write it down exactly, without leaving out a single letter. 
