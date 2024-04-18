da1be4db16abdd645a2d88af6e643a127e87e0c2
atfot <104991246+atfot@users.noreply.github.com>
2024-04-18 20:34:26 +0900

    ㅁㄴㅇㄹ

--- pages/test_2.py
+++ pages/test_2.py
@@ -249,7 +249,7 @@ def main():
                 
                 **Conversation content**: [{st.session_state.conversations}]
 
-                **Three possible answers from a korean psychotherapist who wants to know about his patient first and heal the patient's mind second**: 
+                **Three possible answers from a korean psychotherapist who wants to know about his patient**: 
                 "[{msg}]"
 
                 - After reading the informations above, please **pick the best response from three possible answers** and write it down exactly, without leaving out a single letter. 
