ff2c9fbd928cc16a80349fec8727ccb80e6fe650
atfot <104991246+atfot@users.noreply.github.com>
2024-04-19 15:27:46 +0900

    ㄴㅇ로

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ if 'messages' not in st.session_state:
         with col2:
             st.markdown("# ᆞNᆞᆞEᆞᆞRᆞᆞIᆞ")
         st.title('')
-        col1,col2=st.columns([23.5,5])
+        col1,col2=st.columns([23.6,5])
         with col2:
             st.write("로그인 해주세요")
         username = st.text_input("아이디")
