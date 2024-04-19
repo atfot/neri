383d14b84083642cc4bc11f756de38659bc88824
atfot <104991246+atfot@users.noreply.github.com>
2024-04-19 15:32:20 +0900

    awet

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,52 @@ if 'messages' not in st.session_state:
     col1,col2=st.columns([6,4])
     with col1:
         language_selection=st.toggle('한국어/English')
+        
+    if not language_selection: 
+        st.session_state['korean_mode']=1
+        st.write('한글 모드')
+        col1,col2,col3=st.columns([1.5,7,1.5])
+        with col2:
+            st.markdown("# ᆞNᆞᆞEᆞᆞRᆞᆞIᆞ")
+        st.title('')
+        col1,col2=st.columns([23.5,5])
+        with col2:
+            st.write("로그인 해주세요")
+        username = st.text_input("아이디")
+        password = st.text_input("비밀번호", type="password")
+
+        col1, col2 = st.columns([9.8,1.3])
+        with col1:
+            if st.button("새로 오신 분", type="secondary"):
+                st.session_state.signin = True
+        with col2:
+            if st.button("로그인", type="primary"):
+                if username == st.session_state.user_id and password == st.session_state.password:
+                    st.session_state.logged_in = True
+                elif username == 'test' and password == 'test':
+                    st.session_state.logged_in = True
+                else:
+                    st.session_state.login_error = True
+        if st.session_state.get("logged_in", True):
+            col, col2, col3 = st.columns([3,4,3])
+            with col2:
+                st.success("성공적으로 로그인 되었습니다!")
+                sleep(0.5)
+                st.switch_page("pages/korean_chatbot_2.py")
+        if st.session_state.get('login_error', True):
+            col, col2, col3 = st.columns([2,6,2])
+            with col2:
+                st.error("유저 이름 또는 패스워드가 맞지 않습니다.")
+        if st.session_state.get("login_error", False):
+            pass
+        if st.session_state.get('signin', True):
+            col, col2, col3 = st.columns([3,4,3])
+            with col2:
+                st.success("네리에 오신 것을 환영합니다!")
+                sleep(0.5)
+                st.switch_page("pages/signin.py")
+        if st.session_state.get("signin", False):
+            pass
     if language_selection: 
         st.session_state['korean_mode']=0
         st.write('English mode')        
@@ -67,51 +113,7 @@ if 'messages' not in st.session_state:
         if st.session_state.get("signin", False):
             pass
 
-    if not language_selection: 
-        st.session_state['korean_mode']=1
-        st.write('한글 모드')
-        col1,col2,col3=st.columns([1.5,7,1.5])
-        with col2:
-            st.markdown("# ᆞNᆞᆞEᆞᆞRᆞᆞIᆞ")
-        st.title('')
-        col1,col2=st.columns([23.5,5])
-        with col2:
-            st.write("로그인 해주세요")
-        username = st.text_input("아이디")
-        password = st.text_input("비밀번호", type="password")
-
-        col1, col2 = st.columns([9.8,1.3])
-        with col1:
-            if st.button("새로 오신 분", type="secondary"):
-                st.session_state.signin = True
-        with col2:
-            if st.button("로그인", type="primary"):
-                if username == st.session_state.user_id and password == st.session_state.password:
-                    st.session_state.logged_in = True
-                elif username == 'test' and password == 'test':
-                    st.session_state.logged_in = True
-                else:
-                    st.session_state.login_error = True
-        if st.session_state.get("logged_in", True):
-            col, col2, col3 = st.columns([3,4,3])
-            with col2:
-                st.success("성공적으로 로그인 되었습니다!")
-                sleep(0.5)
-                st.switch_page("pages/korean_chatbot_2.py")
-        if st.session_state.get('login_error', True):
-            col, col2, col3 = st.columns([2,6,2])
-            with col2:
-                st.error("유저 이름 또는 패스워드가 맞지 않습니다.")
-        if st.session_state.get("login_error", False):
-            pass
-        if st.session_state.get('signin', True):
-            col, col2, col3 = st.columns([3,4,3])
-            with col2:
-                st.success("네리에 오신 것을 환영합니다!")
-                sleep(0.5)
-                st.switch_page("pages/signin.py")
-        if st.session_state.get("signin", False):
-            pass
+    
 
     #if 'messages' in st.session_state:
         #if st.session_state['korean_mode']==0:
