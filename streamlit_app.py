27160af1dd4e545b686a7bc2eab998105c5ff00f
atfot <104991246+atfot@users.noreply.github.com>
2024-04-19 15:20:39 +0900

    ㅁㄴㅇㅇㄹ

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if 'messages' not in st.session_state:
         username = st.text_input("ID")
         password = st.text_input("Password", type="password")
 
-        col1, col2 = st.columns([12,1])
+        col1, col2 = st.columns([11.5,1.4])
         with col1:
             if st.button("New User", type="secondary"):
                 st.session_state.signin = True
