41c368bf46f7e6db74d56aa879127a7827790091
atfot <104991246+atfot@users.noreply.github.com>
2024-04-22 19:35:46 +0900

    adw

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ if 'messages' not in st.session_state:
     )
 
 if 'screen_setting' not in st.session_state:
-    x = streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH',  want_output = True)
+    x = streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)
     if x is not None:
-        if x<662:
-            st.session_state.screen_setting='mobile'
-        else: 
-            st.session_state.screen_setting='pc'
+        if x < 662:
+            st.session_state.screen_setting = 'mobile'
+        else:
+            st.session_state.screen_setting = 'pc'
 
     st.session_state.logged_in = False
     st.session_state.signin = False
