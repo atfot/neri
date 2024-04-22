d1e5249d65eb72d716fd5a27b31f038b47921527
atfot <104991246+atfot@users.noreply.github.com>
2024-04-22 19:39:53 +0900

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,22 @@ import streamlit as st
 from time import sleep
 from streamlit_js_eval import streamlit_js_eval
 
-if 'messages' not in st.session_state:
-    st.set_page_config(
+st.set_page_config(
         page_title="Your AI Therapist, Neri",
         page_icon="🧊",
         layout="centered",
         menu_items=None
     )
+if 'screen_setting' not in st.session_state:
+    x=''
+    x = streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)
+    if x < 662:
+        st.session_state.screen_setting = 'mobile'
+    else:
+        st.session_state.screen_setting = 'pc'
 
-    if 'screen_setting' not in st.session_state:
-        x = streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)
-        if x < 662:
-            st.session_state.screen_setting = 'mobile'
-        else:
-            st.session_state.screen_setting = 'pc'
 
+if 'messages' not in st.session_state:
         st.session_state.logged_in = False
         st.session_state.signin = False
         st.session_state.login_error = False
