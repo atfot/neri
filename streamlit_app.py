0226a007bca4217fad20eec56a8d5c1fec0caf54
atfot <104991246+atfot@users.noreply.github.com>
2024-04-23 17:56:53 +0900

    afw

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from time import sleep
+from streamlit_js_eval import streamlit_js_eval
 
 if 'messages' not in st.session_state:
+    if 'screen_setting' not in st.session_state:
+        st.session_state.screen_setting = 'pc'  # default value
+
+    if (x := streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)) is not None:
+        st.session_state.screen_setting = 'mobile' if x < 662 else 'pc'
+        
     st.set_page_config(
     page_title="Your AI Therapist, Neri",
     page_icon="🧊",
