08946f5498002740f89c4d5322e42db27dc4f44f
atfot <104991246+atfot@users.noreply.github.com>
2024-04-23 14:54:27 +0900

    ㅎㄷㄱ

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,7 @@ if 'messages' not in st.session_state:
                 st.success("성공적으로 로그인 되었습니다!")
                 sleep(0.5)
                 st.switch_page("pages/korean_chatbot_2.py")
+                
         if st.session_state.get('login_error', True):
             col, col2, col3 = st.columns([2,6,2])
             with col2:
