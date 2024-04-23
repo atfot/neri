408c6228d547fb51c56085acf3547820d45b5df6
atfot <104991246+atfot@users.noreply.github.com>
2024-04-23 14:52:37 +0900

    agw

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if 'messages' not in st.session_state:
         
         username = st.text_input("아이디")
         password = st.text_input("비밀번호", type="password")
-
+"""
         if st.session_state.screen_setting=='pc':
             col1, col2, col3 = st.columns([2,6,2])
             with col1:
@@ -62,7 +62,25 @@ if 'messages' not in st.session_state:
                     st.session_state.login_error = True
             if st.button("새로 오신 분", type="secondary",use_container_width=True):
                     st.session_state.signin = True
-            
+"""
+        if st.button("로그인", type="primary",use_container_width=True):
+            if username == st.session_state.user_id and password == st.session_state.password:
+                st.session_state.logged_in = True
+            elif username == 'test' and password == 'test':
+                st.session_state.logged_in = True
+            else:
+                st.session_state.login_error = True    
+        col1, col2, col3 = st.columns([3.3,3.3,3.4])
+        with col1:
+            if st.button("비밀번호 찾기", type="secondary",use_container_width=True):
+                st.write('hi')
+        with col2:
+            if st.button("아이디", type="secondary",use_container_width=True):
+                st.write('hi')
+        with col3:
+            if st.button("새로 오신 분", type="secondary",use_container_width=True):
+                st.session_state.signin = True
+        
         if st.session_state.get("logged_in", True):
             col, col2, col3 = st.columns([3,4,3])
             with col2:
