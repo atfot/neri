4f761e4715729ef201c428337ab101b65c6e1460
atfot <104991246+atfot@users.noreply.github.com>
2024-04-24 23:38:41 +0900

    awe

--- korean_navigation.py
+++ korean_navigation.py
@@ -21,8 +21,8 @@ def make_sidebar():
         st.write("")
 
         if st.session_state.get("logged_in", True):
-            st.page_link("pages/korean_analysis.py", label="나의 노력의 결과는?", icon="🏋️")
             st.page_link("pages/korean_chatbot_2.py", label="당신의 카운셀러", icon="💛")
+            st.page_link("pages/korean_analysis.py", label="내 정보", icon="ℹ️")
 
             st.write("")
             st.write("")
