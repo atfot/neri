cf479a6feb3c795cc39ee7b187001600a8beb9ce
atfot <104991246+atfot@users.noreply.github.com>
2024-04-24 14:30:22 +0900

    eter

--- pages/page2.py
+++ pages/page2.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from korean_navigation import make_sidebar
+
+st.set_page_config(
+    page_title="Your AI Therapist, Neri",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+make_sidebar()
 
 st.write('hi')
