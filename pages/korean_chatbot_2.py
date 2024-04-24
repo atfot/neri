eb1fd70ce49797d3ed23296593a63aa792f7aede
atfot <104991246+atfot@users.noreply.github.com>
2024-04-24 16:29:47 +0900

    ㅁㅈㄷㄱㅈㄷ

--- pages/korean_chatbot_2.py
+++ pages/korean_chatbot_2.py
@@ -349,7 +349,7 @@ def main():
             col1,col2=st.columns([9,1])
             with col1:
                 st.chat_message('assistant').write(st.session_state.messages[-1]['content'])
-                st.session_state.conversations
+                #st.session_state.conversations
             with col2:
                 st.write('')
                 st.button('🔄', on_click=reply_again_cb)
