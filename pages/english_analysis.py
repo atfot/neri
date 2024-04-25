94d8885f55873c8b3bff455d333e2b53033d168d
atfot <104991246+atfot@users.noreply.github.com>
2024-04-25 17:09:35 +0900

    awe

--- pages/english_analysis.py
+++ pages/english_analysis.py
@@ -1,16 +1,112 @@
-import streamlit as st
-from english_navigation import make_sidebar
-from openai import OpenAI
-import time
+if 'my_info' not in st.session_state:
+  with st.spinner('# Now Loading...'):
+    problem_analysis = st.session_state.client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                messages=[
+                  {
+                    "role": "system",
+                    "content": """Your role as a professional psychotherapist is to score the extent to which the client's problem has improved given the information below and explain why.
+                    
+                    **Remember**:
+                    1. Your score should be much lower than you think.
+                    2. You should never speak rudely.
+                    """
+                  },
+                  {
+                    "role": "user",
+                    "content": f"""
+                    # My Request:
+                    From a professional psychotherapist's perspective, score the extent to which the client's problem is improved by the information given below and explain why.
 
-st.set_page_config(
-    page_title="Your AI Therapist, Neri",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
-make_sidebar()
+                    # Informations you need to know
+                    - Client's Name : {st.session_state.username}
+                    - Age : {st.session_state.age}
+                    - Gender : {st.session_state.gender}
+                    - Problem : {st.session_state.problem}
+                    - Problem Explanation : {st.session_state.problem_explanation}
+                    - Goal : {st.session_state.goal}
 
-if 'client' not in st.session_state:
-  st.session_state.client = OpenAI(api_key=st.secrets['api_key'])
+                    - Message summary : 
+                    {st.session_state.message_summary}
 
+                    - The latest conversations:
+                    {st.session_state.conversations}
+                  
+                    # Answer form
+                    - You need to use the form below to answer my request using Korean language.
+                    '''
+                    Analysis : [Analyze the information I've given you by not using any bullet points.]
+
+                    Score : [Based on the analysis you did, please score how well the {st.session_state.username}'s problem was solved.]
+
+                    Explanation : [Tell me how the score you gave me was based on your considerations.
+                    *Scoring criteria*:
+                    10 : The person's psychosis has been cured, or the client is no longer suffering from the problem.
+                    9 : The person's mental illness is on the verge of being cured or the issue is on the verge of being completely resolved.
+                    6 ~ 8 : The client is directly demonstrating a willingness to work toward a positive direction.
+                    3 ~ 5 : The client is not directly demonstrating a willingness to move in a positive direction.
+                    2 : The client is directly demonstrating a willingness to work toward a negative direction.
+                    1 : The client has a serious mental illness or mental health issue and needs to see a real doctor or psychologist to address it.]
+
+                    Best thing to do : [Tell me what you think is the best thing for {st.session_state.username} to do in that situation,using a bullet point summary, as a professional psychologist.]
+                    '''
+                    **Remember**:
+                    1. Your score should be much lower than you think.
+                    2. Don't use the word 'client'.
+                    3. If you need to use the word 'client', don't use that word and replace it into the client's name, such as {st.session_state.username}.
+                    4. You should never speak rudely.
+        """
+                  }
+                ],
+                temperature=1,
+                max_tokens=1024,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+                )    
+    problem_analysis = problem_analysis.choices[0].message.content
+    problem_analysis=problem_analysis.strip().strip("'''")
+    st.session_state.problem_analysis=problem_analysis
+    problem_analysis=problem_analysis[problem_analysis.find(':')+1:].strip()
+    st.session_state.client_analysis=problem_analysis[:problem_analysis.find('\n')]
+    problem_analysis=problem_analysis[problem_analysis.find('\n'):].strip()
+    problem_analysis=problem_analysis[problem_analysis.find(':')+1:].strip()
+    st.session_state.score=problem_analysis[:problem_analysis.find('\n')]
+    problem_analysis=problem_analysis[problem_analysis.find('\n'):].strip()
+    problem_analysis=problem_analysis[problem_analysis.find(':')+1:].strip()
+    st.session_state.score_explanation=problem_analysis[:problem_analysis.find('\n')]
+    problem_analysis=problem_analysis[problem_analysis.find('\n'):].strip()
+    problem_analysis=problem_analysis[problem_analysis.find(':')+1:].strip()
+    st.session_state.what_to_do=problem_analysis.split('\n')
+    st.session_state.my_info=True
+
+if 'my_info' in st.session_state:
+  st.title('My Info')
+
+  col1,col2,col3=st.columns([4,1,5])
+  with col1:
+    st.write(f"""1. Your Name: {st.session_state.username}
+  2. Age: {st.session_state.age}
+  3. Gender: {st.session_state.gender}
+  4. Problem : {st.session_state.problem}
+  5. Problem Explanation: {st.session_state.problem_explanation}
+  6. Goal : {st.session_state.goal}"""
+  )  
+    st.title('')
+  with col3:
+    month=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    st.subheader(f"Analysis results on {month}[{time.localtime().tm_mon}+1] {time.localtime().tm_mday}, {time.localtime().tm_year}")
+    st.write(f'Problem Analysis : {st.session_state.client_analysis}')
+    st.write(f'Score : {st.session_state.score}')
+    st.write(f'Score Explanation : {st.session_state.score_explanation}')
+    st.title('')
+  st.write('Actions that might help you : ')
+  for i in st.session_state.what_to_do:
+    st.write(i)
+  col1,col2=st.columns([8,2])
+  with col2:
+      if st.button('Fix my Info',use_container_width=True):
+        st.switch_page('pages/signin.py')
+  #st.write(st.session_state.problem_analysis)
+  #st.write(st.session_state.conversations)
+  #st.write(st.session_state.message_summary)
--- pages/english_chatbot.py
+++ pages/english_chatbot.py
@@ -12,3 +12,313 @@ make_sidebar()
 
 if 'client' not in st.session_state:
   st.session_state.client = OpenAI(api_key=st.secrets['api_key'])
+
+if 'username' not in st.session_state:
+   st.session_state.username=st.secrets.user_name
+   st.session_state.age=st.secrets.age
+   st.session_state.gender=st.secrets.user_gender
+   st.session_state.gender=st.secrets.user_gender
+   st.session_state.problem=st.secrets.problem
+   st.session_state.problem_explanation=st.secrets.problem_explanation
+   st.session_state.goal=st.secrets.goal
+
+# variables
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "Psychotherapist", "content": "What's bothering you?"}]
+    st.session_state['conversations']=[{"role": "Psychotherapist", "content": "What's bothering you?"}]   
+    st.session_state['message_summary'] = 'Nothing has been written to date, and the conversation starts below.'
+
+if 'repeat' not in st.session_state:
+    st.session_state.repeat = False
+
+# functions
+def reply_again_cb():
+    st.session_state.repeat = True
+
+if st.session_state.repeat==True:
+    st.session_state.messages=st.session_state.messages[:-1]
+    st.session_state.conversations=st.session_state.conversations[:-1]
+
+def main():
+
+    # Print msg history.
+    last_user_message = None
+    for message in st.session_state.messages:
+
+        # Print the user msg if it is not repeating successively.
+        if (last_user_message is not None and
+            message['role'] == "Psychotherapist" and
+            last_user_message == message["content"]
+        ):
+            pass
+        else:
+            # Print both msgs from user and assistant
+            if message['role']=="Psychotherapist":
+                st.chat_message('assistant').write(message["content"])
+            if message['role']=="Mental patient":
+                st.chat_message('user').write(message["content"])
+
+        # Backup last user msg used to identify successive same user content.
+        if message['role'] == "Mental patient":
+            last_user_message = message["content"]
+
+    if prompt := st.chat_input("Feel free to tell me all your problems.") or st.session_state.repeat:
+        def text_logic():
+            if st.session_state.repeat==True:
+                pass
+            else:
+                st.session_state.messages.append({"role": "Mental patient", "content": prompt})
+                st.session_state.conversations.append({"role": "Mental patient", "content": prompt})
+            if len(st.session_state.messages)%3==0:
+                summary = st.session_state.client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                messages=[
+                    {
+                    "role": "system",
+                    "content": "Your role is to summarize the paragraph I give to you."
+                    },
+                    {
+                    "role": "user",
+                    "content": f"""                    
+Please briefly summarize the conversation below.
+
+{st.session_state.messages}"""
+                    }
+                ],
+                temperature=1,
+                max_tokens=1024,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+                )
+                st.session_state['message_summary'] = summary.choices[0].message.content
+                st.session_state['conversations'] = st.session_state.messages[-3:]
+            
+            progress_text='thinking...'
+            my_bar=st.progress(0,text=progress_text)
+            
+            system_prompt=f"""```
+                # Primary Assistant Guidance
+                Your goal is to help me, the playwright, write a script for a play. Let's go step-by-step:
+
+                # Information about the play
+                - Conversation between one mentally ill person and one psychotherapist
+                - The two are now meeting and talking online
+                - Psychologist uses the most respectful tone of voice possible, and the person with mental illness prefers it
+                - Person with mental illness want empathy and comfort for him/herself
+                - The psychotherapist wants to heal the mentally ill person by building a strong relationship with them
+
+                # Character information
+                1. mentally ill person
+                - Name: {st.session_state.username}
+                - Age: {st.session_state.age}
+                - Gender: {st.session_state.gender}
+                - Problem : {st.session_state.problem}
+                - Problem Explanation: {st.session_state.problem_explanation}
+                - Goal : {st.session_state.goal}
+
+                2. psychological counselor
+                - Name : Neri
+                - Age : 55 years old
+                - Gender: Male
+                - Country of Origin : South Korea
+                - City of residence : Seoul
+                - Characteristics : Neri knows the information of {st.session_state.username}, a mentally ill person, and conducts psychotherapy based on it
+
+                **REMEMBER**: 
+                '''
+                - Psychotherapist cannot speak information from mentally ill person and himself unless it's really necessary.
+                - Keep in mind that the psychotherapist's response is part of the conversation and will be followed by the mentally ill person's response
+                - The psychotherapist's response should fit the tone and content of the conversation
+                - If you get a short answer from the mental patient, ask him/her a related question
+                - The psychotherapist is talking to only one person with a mental illness(Check the "# Character information")
+                - Never reuse any sentences that has a same context which have already been used within a conversation
+                - The grammar of the sentences should be perfect
+                - If you get any questions from the mental patient, give him/her an answer
+                - Never use a tone that suggests you want to do something with the patient
+                - Make sure you understand the content of "# Information about the play" and "# Character information" before answering
+                '''   
+                ```
+            """
+            
+            user_prompt_1=f"""
+                ```
+                # My requests: 
+                Your goal is to help me, the playwright, write a script for a play. Let's go step-by-step:
+                - Please read the form below step by step and answer the questions in the exact form below
+
+                - Read this step by step before filling out the form
+                **Summary of the conversation**: [{st.session_state.message_summary}]
+                **Latest Conversations**: [{st.session_state.conversations}]     
+                
+                - This is the form      
+                '''
+                **Three possible answers from a psychotherapist**: 
+                [Given the above summary and the conversation, what are 3 possible answers a psychotherapist might give here?]
+                '''
+                
+                **REMEMBER**: 
+                - The grammar of the sentences should be perfect.
+                - Never use a tone that suggests you want to do something with the patient.
+                - If you get a short answer from the mental patient, ask him/her a related question.
+                - Never reuse any sentences that has a same context which have already been used within a conversation.
+                - If you get any questions from the mental patient, give him/her an answer.
+                ```
+
+            """
+            
+            response = st.session_state.client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+            {
+                "role": "system",
+                "content": f"{system_prompt}"
+            },
+            {
+                "role": "user",
+                "content": f"{user_prompt_1}"
+            }
+            ],
+            temperature=1,
+            max_tokens=1028,
+            top_p=0.9,
+            frequency_penalty=1,
+            presence_penalty=1
+        )
+            my_bar.progress(25,text=progress_text)
+            msg = response.choices[0].message.content
+            sentence_selection = st.session_state.client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+            {
+                "role": "system",
+                "content": """Your role is to read the dialogue, summary, and examples of the three answers and choose the best sentence from the three.
+                
+                **REMEMBER**:
+                1. After you pick the best response, then write it down exactly, without leaving out a single letter.
+                2. **There should be no "" marks in your answer, and no : or - marks to show the answer.**
+                3. Submit the original sentences that I gave you if there is no grammar problem.
+                4. Never attach embellishments or explanation to your answers. Submit only **context** as output. 
+                5. Don't use any words or phrases other than the context.
+                6. Never use a tone that suggests you want to do something with the patient.
+                7. Never reuse any sentences that has a same context which have already been used within a conversation.
+                8. If you get any questions from the mental patient, give him/her an answer.
+                9. Never choose the sentence that contains 'How does it feel' or anything resembles that.
+                """
+            },
+            {
+                "role": "user",
+                "content": f"""
+                # My request:
+                Read the summary, dialogue, and examples of the three answers and choose the best sentence from the three. Lets go step by step-
+
+                - Read these informations carefully before answering my question.
+                **Summary of the conversation**: [{st.session_state.message_summary}]
+                
+                **Conversation content**: [{st.session_state.conversations}]
+
+                **Three possible answers from a psychotherapist who wants to learn about his patient and heal the patient's mind**: 
+                "[{msg}]"
+
+                - After reading the informations above, please **pick the best response from three possible answers** considering psychotherapist's intention, and write it down exactly, without leaving out a single letter. 
+                
+                **REMEMBER**:
+                1. After you pick the best response, then write it down exactly, without leaving out a single letter.
+                2. **There should be no "" marks in your answer, and no : or - marks to show the answer.**
+                3. Submit the original sentences that I gave you if there is no grammar problem.
+                4. Never attach embellishments or explanation to your answers. Submit only **context** as output. 
+                5. Don't use any words or phrases other than the context.
+                6. Never use a tone that suggests you want to do something with the patient.
+                7. Never reuse any sentences that has a same context which have already been used within a conversation.
+                8. If you get any questions from the mental patient, give him/her an answer.
+                9. Never choose the sentence that contains 'How does it feel' or anything resembles that.]
+        """
+            }
+            ],
+            temperature=1,
+            max_tokens=512,
+            top_p=1,
+            frequency_penalty=1,
+            presence_penalty=1
+        )
+            my_bar.progress(50,text=progress_text)
+            selected_msg = sentence_selection.choices[0].message.content.strip('"')
+            humanize_sentence = st.session_state.client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+            {
+                "role": "system",
+                "content": """
+                Your role is to rephrase the sentences I give you as if they were spoken by a real person in the middle of a conversation.
+      
+                **REMEMBER**:
+                1. After you pick the best response, then write it down exactly, without leaving out a single letter.
+                2. **There should be no "" marks in your answer, and no : or - marks to show the answer.**
+                3. Submit the original sentences that I gave you if there is no grammar problem.
+                4. Never attach embellishments or explanation to your answers. Submit only **context** as output. 
+                5. Don't use any words or phrases other than the context.
+                6. Never reuse any sentences that has a same context which have already been used within a conversation.
+                7. Never choose the sentence that contains 'How does it feel' or anything resembles that.
+                """
+            },
+            {
+                "role": "user",
+                "content": f"""
+                # My Request: Rephrase the sentences below.
+      
+                **REMEMBER**:
+                1. **There should be no "" marks in your answer, and no : or - marks to show the answer.**
+                2. Submit the original sentences that I gave you if there is no grammar problem.
+                3. Never attach embellishments or explanation to your answers. Submit only **context** as output. 
+                4. Don't use any words or phrases other than the context.
+
+                '''
+                {selected_msg}
+                '''
+        """
+            }
+            ],
+            temperature=1,
+            max_tokens=512,
+            top_p=1,
+            frequency_penalty=1,
+            presence_penalty=1
+        )
+            my_bar.progress(75,text=progress_text)
+            try:
+                humanize_msg = sentence_selection.choices[0].message.content
+                humanize_msg = humanize_msg[humanize_msg.index(':')+1:].strip(' ').strip('"')
+            except:
+                humanize_msg = sentence_selection.choices[0].message.content.strip('"')
+            st.session_state.messages.append({"role": "Psychotherapist", "content": humanize_msg})
+            st.session_state.conversations.append({"role": "Psychotherapist", "content": humanize_msg})
+            my_bar.progress(100,text=progress_text)
+            my_bar.empty()
+
+        # Get the last user prompt in the msg history.
+        if st.session_state.repeat:
+            prompt = st.session_state.messages[-1]['content']
+            text_logic()
+            col1,col2=st.columns([9,1])
+            with col1:
+                st.chat_message('assistant').write(st.session_state.messages[-1]['content'])
+            with col2:
+                st.write('')
+                st.button('🔄', on_click=reply_again_cb)
+            st.session_state.repeat = False  # reset
+            #st.write(st.session_state.messages[:-1])                
+        else:
+            # Only print the user msg if repeat is false.
+            st.chat_message('user').write(prompt)
+            text_logic()
+            col1,col2=st.columns([9,1])
+            with col1:
+                st.chat_message('assistant').write(st.session_state.messages[-1]['content'])
+            with col2:
+                st.write('')
+                st.button('🔄', on_click=reply_again_cb)
+            #st.write(st.session_state.messages[:-1])
+
+
+if __name__ == '__main__':
+    main()
