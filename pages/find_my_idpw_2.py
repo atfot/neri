5dbac1810de2fbaf5aebae1f38bee6bd7b47c340
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 18:27:41 +0900

    atwe

---
+++ pages/find_my_idpw_2.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import time
+
+st.set_page_config(
+        page_title="Your AI Therapist, Neri",
+        page_icon="🧊",
+        layout="wide",
+        menu_items=None
+    )
+
+if 'user_id' not in st.session_state:
+   st.session_state.user_id='test'
+   st.session_state.password='test'
+if 'username' not in st.session_state:
+    st.session_state.username=st.secrets.user_name
+
+st.session_state.filled_input=0
+
+if 'korean_mode' not in st.session_state:
+    st.switch_page('streamlit_app.py')
+
+
+if st.session_state.korean_mode==1:
+    button=st.button("메인 화면으로")
+    if button:
+        del st.session_state.filled_input
+        st.switch_page("streamlit_app.py")
+            
+
+if st.session_state.korean_mode==0:
+    button=st.button("Go to main", "https://neriuut.streamlit.app/")
+    if button:
+        st.switch_page("streamlit_app.py")
+    col1,col2=st.columns([5,5])
+    with col1:
+        st.markdown('<center><h3>Fix your ID</h3></center>', unsafe_allow_html=True)
+    with col2:
+        st.markdown('<center><h3>Fix your PW</h3></center>', unsafe_allow_html=True)
+    if st.button('Do you wanna save your modifications?'):
+        st.success('Your modifications have been applied!')
+        st.write(st.session_state.user_id)
+        st.write(st.session_state.password)
+        time.sleep(5)
+        st.switch_page('streamlit_app.py')
