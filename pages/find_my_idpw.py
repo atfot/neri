691706c832969d6a5c2ba9011489aa2ad02ced51
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 16:17:03 +0900

    awte

--- pages/find_my_idpw.py
+++ pages/find_my_idpw.py
@@ -26,12 +26,14 @@ if st.session_state.korean_mode==1:
         st.write('사용하시던 닉네임과 패스워드를 적어주세요')
     with col2:
         st.markdown('<center><h3>패스워드 수정</h3></center>', unsafe_allow_html=True)
-    if st.button('이대로 저장할까요?'):
-        st.success('수정 내역이 저장되었습니다!')
-        st.write(st.session_state.user_id)
-        st.write(st.session_state.password)
-        time.sleep(5)
-        st.switch_page('streamlit_app.py')
+    col1,col2,col3=st.columns(2,6,2)
+    with col2:
+        if st.button('이대로 저장할까요?',):
+            st.success('수정 내역이 저장되었습니다!')
+            st.write(st.session_state.user_id)
+            st.write(st.session_state.password)
+            time.sleep(5)
+            st.switch_page('streamlit_app.py')
 
 if st.session_state.korean_mode==0:
     button=st.button("Go to main", "https://neriuut.streamlit.app/")
