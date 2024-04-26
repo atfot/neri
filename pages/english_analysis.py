a3cda4dec0ee9940c57aa91a5d7393d2762775a2
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 21:00:21 +0900

    wra

--- pages/english_analysis.py
+++ pages/english_analysis.py
@@ -198,6 +198,7 @@ if st.session_state.fix_info==False:
   for i in st.session_state.what_to_do:
     st.write(i)
   st.title('')
+  st.markdown('<p><h3><center>Problem Resolution Graph</center></h3></p>', unsafe_allow_html=True)
   if time.localtime().tm_mon<10:
       z=f'0{time.localtime().tm_mon}'
   else:
