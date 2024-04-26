9f7e7ffa7e3412e148ff07e4766bc912ed60b738
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 15:58:24 +0900

    wer

--- pages/find_my_idpw.py
+++ pages/find_my_idpw.py
@@ -20,9 +20,9 @@ if st.session_state.korean_mode==1:
         st.switch_page("streamlit_app.py")
     col1,col2=st.columns([5,5])
     with col1:
-        st.markdown('<center><h5>아이디 수정</h5></center>', unsafe_allow_html=True)
+        st.markdown('<center><h3>아이디 수정</h3></center>', unsafe_allow_html=True)
     with col2:
-        st.markdown('<center><h5>패스워드 수정</h5></center>', unsafe_allow_html=True)
+        st.markdown('<center><h3>패스워드 수정</h3></center>', unsafe_allow_html=True)
 if st.session_state.korean_mode==0:
     button=st.button("Go to main", "https://neriuut.streamlit.app/")
     if button:
@@ -32,6 +32,6 @@ if st.session_state.korean_mode==0:
         st.switch_page("streamlit_app.py")
     col1,col2=st.columns([5,5])
     with col1:
-        st.markdown('<center><h5>Fix your ID</h5></center>', unsafe_allow_html=True)
+        st.markdown('<center><h3>Fix your ID</h3></center>', unsafe_allow_html=True)
     with col2:
-        st.markdown('<center><h5>Fix your PW</h5></center>', unsafe_allow_html=True)
+        st.markdown('<center><h3>Fix your PW</h3></center>', unsafe_allow_html=True)
