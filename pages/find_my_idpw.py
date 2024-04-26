b7b0a82cbc8dd1c7258127025e64cd4fbbf6dc89
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 17:09:26 +0900

    rwae

--- pages/find_my_idpw.py
+++ pages/find_my_idpw.py
@@ -16,14 +16,13 @@ if 'user_id' not in st.session_state:
 if 'korean_mode' not in st.session_state:
     st.switch_page('streamlit_app.py')
 
-if 'fix_id' not in st.session_state:
-    st.session_state.fix_id=False
+if 'save_button' not in st.session_state:
     st.session_state.save_button=False
 
 if st.session_state.korean_mode==1:
     button=st.button("메인 화면으로")
     if button:
-        del st.session_state.fix_id, st.session_state.save_button
+        del st.session_state.save_button
         st.switch_page("streamlit_app.py")
     col1,col2,col3=st.columns([4.5,1,4.5])
     with col1:
@@ -32,37 +31,24 @@ if st.session_state.korean_mode==1:
             st.write('사용하시던 닉네임과 패스워드를 적어주세요.')
         else:
             st.write('새로 사용하실 아이디를 적어주세요.')
-        if st.session_state.fix_id==False:
-            nickname=st.text_input('닉네임',key='nickname')
-            password=st.text_input('패스워드',type='password',key='password')
-            id_confirm_button=st.button('확인', key='confirm_id')
-            x=0
-            if nickname=='test':
-                x+=1
-            if password=='test':
-                x+=1
-            if id_confirm_button:
-                if x==2:
-                    st.session_state.fix_id=True
-                else:
-                    st.write('빈칸을 전부 채워주세요.')
-        if st.session_state.fix_id==True:
-            new_id=st.text_input('새로 사용할 ID',key='new_id')
-            new_id_check=st.text_input('다시 한번 적어주세요',type='password',key='new_id_check')
-            new_confirm_button=st.button('확인',key='confirm_new_id')
-            x=0
-            if new_id:
-                x+=1
-            if new_id_check:
-                x+=1
-            if new_confirm_button:
-                if new_id==new_id_check:
-                    if x==2:
-                        st.session_state.fix_id=False
-                        st.session_state.user_id=new_id
-                        st.session_state.save_button=True
+        nickname=st.text_input('닉네임',key='nickname')
+        password=st.text_input('패스워드',type='password',
+        key='password')
+        new_id=st.text_input('새로 사용할 ID',key='new_id')
+        new_id_check=st.text_input('다시 한번 적어주세요',type='password',key='new_id_check')
+        st.session_state.filled_input=0
+        if nickname=='test':
+            st.session_state.filled_input+=1
+        if password=='test':
+            st.session_state.filled_input+=1
+        if new_id:
+            st.session_state.filled_input+=1
+        if new_id_check:
+            st.session_state.filled_input+=1        
     with col3:
         st.markdown('<center><h3>패스워드 수정</h3></center>', unsafe_allow_html=True)
+    if st.session_state.filled_input==4 or st.session_state.filled_input==8:
+        st.session_state.save_button=True   
     if st.session_state.save_button==True:
         col1,col2,col3=st.columns([2,6,2])
         with col2:
@@ -72,11 +58,9 @@ if st.session_state.korean_mode==1:
                     st.write(st.session_state.user_id)
                 if st.session_state.password:
                     st.write(st.session_state.password)
-                del st.session_state.fix_id, st.session_state.save_button
                 time.sleep(5)
+                del st.session_state.save_button
                 st.switch_page('streamlit_app.py')
-    else:
-        pass
 
 if st.session_state.korean_mode==0:
     button=st.button("Go to main", "https://neriuut.streamlit.app/")
