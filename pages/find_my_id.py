06d2fb8888d82c1243802e56322161ae29f6ad34
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 19:28:05 +0900

    afs

--- pages/find_my_id.py
+++ pages/find_my_id.py
@@ -4,7 +4,7 @@ import time
 st.set_page_config(
         page_title="Your AI Therapist, Neri",
         page_icon="🧊",
-        layout="centered",
+        layout="wide",
         menu_items=None
     )
 
@@ -15,7 +15,6 @@ if 'password' not in st.session_state:
 if 'username' not in st.session_state:
     st.session_state.username=st.secrets.user_name
 st.session_state.filled_input=0
-st.session_state.save_success=False
 
 if 'korean_mode' not in st.session_state:
     st.switch_page('streamlit_app.py')
@@ -26,8 +25,9 @@ if st.session_state.korean_mode==1:
     if button:
         del st.session_state.filled_input
         st.switch_page("streamlit_app.py")
-    st.markdown('<center><h3>아이디 수정</h3></center>', unsafe_allow_html=True)
-    with st.form('my_form'):
+    col1,col2,col3=st.columns([4.5,1,4.5])
+    with col1:
+        st.markdown('<center><h3>아이디 수정</h3></center>', unsafe_allow_html=True)
         nickname=st.text_input('닉네임')
         if nickname:
             if nickname!=st.session_state.username:
@@ -52,62 +52,16 @@ if st.session_state.korean_mode==1:
                 st.error('새로운 아이디와 해당 아이디가 서로 다릅니다.')    
             else:
                 st.session_state.filled_input+=1
-        col1,col2=st.columns([8.9,1.1])
-        with col2:
-            submitted=st.form_submit_button('저장',use_container_width=True)
-            if submitted:
-                if st.session_state.filled_input==4:
-                    st.session_state.save_success=True
-                else:
-                    pass
-        if st.session_state.save_success==True:
-            st.success('수정 내역이 저장되었습니다!')
-            if st.session_state.user_id:
-                st.write(st.session_state.user_id)
-            if st.session_state.password:
-                st.write(st.session_state.password)
-            time.sleep(5)
-            del st.session_state.filled_input,st.session_state.save_success
-            st.switch_page('streamlit_app.py')
-        else:
-            pass
-if st.session_state.korean_mode==0:
-    button=st.button("Go to main", "https://neriuut.streamlit.app/")
-    if button:
-        del st.session_state.filled_input
-        st.switch_page("streamlit_app.py")
-    st.markdown('<center><h3>Change your ID</h3></center>', unsafe_allow_html=True)
-    with st.form('my_form'):
-        nickname=st.text_input('Username')
-        if nickname:
-            if nickname!=st.session_state.username:
-                st.error('This is not the username you were using.')
-            else:
-                st.session_state.filled_input+=1
-        password=st.text_input('Password',type='password')
-        if password:
-            if password!=st.session_state.password:
-                st.error('This is not the password you are using.')
-            else:
-                st.session_state.filled_input+=1
-        new_id=st.text_input('New ID',key='new_id')
-        if new_id:
-            if new_id==st.session_state.user_id:
-                st.error('This is the same username you originally used.')
-            else:
-                st.session_state.filled_input+=1
-        new_id_check=st.text_input('Write down the new ID you want to use again.',type='password',key='new_id_check')
-        if new_id_check:
-            if new_id_check!=new_id:
-                st.error('The new ID and the corresponding ID are different.')    
-            else:
-                st.session_state.filled_input+=1
-        col1,col2=st.columns([8.9,1.1])
-        with col2:
-            submitted=st.form_submit_button('Save',use_container_width=True)
-            if submitted:
-                if st.session_state.filled_input==4:
-                    st.success('Your modifications have been saved!')
+
+    with col3:
+        st.markdown('<center><h3>패스워드 수정</h3></center>', unsafe_allow_html=True)
+    
+    col1,col2,col3=st.columns([1,8,1])
+    with col2:
+        st.title('')
+        if st.button('이대로 저장할까요?', type='primary',use_container_width=True):
+                if st.session_state.filled_input==4 or st.session_state.filled_input==8:
+                    st.success('수정 내역이 저장되었습니다!')
                     if st.session_state.user_id:
                         st.write(st.session_state.user_id)
                     if st.session_state.password:
@@ -116,4 +70,20 @@ if st.session_state.korean_mode==0:
                     del st.session_state.filled_input
                     st.switch_page('streamlit_app.py')
                 else:
-                    st.error('Fill in all the blanks.')
+                    pass
+
+if st.session_state.korean_mode==0:
+    button=st.button("Go to main", "https://neriuut.streamlit.app/")
+    if button:
+        st.switch_page("streamlit_app.py")
+    col1,col2=st.columns([5,5])
+    with col1:
+        st.markdown('<center><h3>Fix your ID</h3></center>', unsafe_allow_html=True)
+    with col2:
+        st.markdown('<center><h3>Fix your PW</h3></center>', unsafe_allow_html=True)
+    if st.button('Do you wanna save your modifications?'):
+        st.success('Your modifications have been applied!')
+        st.write(st.session_state.user_id)
+        st.write(st.session_state.password)
+        time.sleep(5)
+        st.switch_page('streamlit_app.py')
