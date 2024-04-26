5e37d731f8693677cf2cdbcef534bd0f249cb77d
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 18:17:23 +0900

    awter

--- pages/find_my_idpw.py
+++ pages/find_my_idpw.py
@@ -60,11 +60,11 @@ if st.session_state.korean_mode==1:
     
     col1,col2,col3=st.columns([1,8,1])
     with col2:
-        if st.session_state.save_button==False:
+        if st.session_state.get('save_button',False):
             if st.button('제출',type='secondary',use_container_width=True):
                 if st.session_state.filled_input==4 or st.session_state.filled_input==8:
                     st.session_state.save_button=True   
-        if st.session_state.save_button==True:
+        if st.session_state.get('save_button',True):
             if st.button('이대로 저장할까요?', type='primary',use_container_width=True):
                 st.success('수정 내역이 저장되었습니다!')
                 if st.session_state.user_id:
