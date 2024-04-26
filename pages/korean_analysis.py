a1436416321ce6d7e0960111ebde3aab10f4e292
atfot <104991246+atfot@users.noreply.github.com>
2024-04-26 20:59:11 +0900

    ter

--- pages/korean_analysis.py
+++ pages/korean_analysis.py
@@ -201,7 +201,7 @@ if st.session_state.fix_info==False:
   for i in st.session_state.what_to_do:
     st.write(i)
   st.title('')
-  st.markdown('<p><center>고민 해결도 그래프</b></center></p>', unsafe_allow_html=True)
+  st.markdown('<p><b><center>고민 해결도 그래프</b></center></p>', unsafe_allow_html=True)
   if time.localtime().tm_mon<10:
       z=f'0{time.localtime().tm_mon}'
   else:
