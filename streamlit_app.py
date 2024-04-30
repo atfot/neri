c86ec934d61daaf7bcfaba8dc03fe5ccd89bb593
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 22:11:43 +0900

    wet

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,12 +23,12 @@ if 'messages' not in st.session_state:
 
 * {
 	font-family: 'Beeunhye';
-    font-size: 1em;
+    font-size: 1.5em;
 }
                 
 p {
 	font-family: 'Beeunhye';
-    font-size: 1em;
+    font-size: 1.5em;
 }
 
 </style>
