bb0746f0f034b32962ca8e4ea1c49c7058c2c862
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 23:19:22 +0900

    sfa

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div > div > label > d
 }
 .st-emotion-cache-10trblm.e1nzilvr1 {
 	font-family: 'Beeunhye';
-    font-size: 1.5em;
+    font-size: 1.75em;
 }
 div.st-bl.st-bm.st-bn.st-bo.st-bp.st-bq.st-br.st-bs.st-bt.st-bu.st-bv > div > div > p{
 	font-family: 'Beeunhye';
