11ec76ca014eff76cd7e570ebb44f2ae6b5befcc
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 13:50:05 +0900

    fsa

--- korean_menu.py
+++ korean_menu.py
@@ -37,9 +37,15 @@ def make_sidebar():
 
 ''')
             st.page_link("pages/korean_instruction.py", label="사용법", icon="❓")
-            st.title('')
+            st.write('''
+
+
+''')
             st.divider()
-            st.title('')
+            st.write('''
+
+
+''')
 
             if st.button("로그아웃",type='primary',use_container_width=True):
                 logout()
