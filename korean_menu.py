3bc6323dac78d094942ab5c1f6904a05b46529a7
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 13:45:39 +0900

    gaw

--- korean_menu.py
+++ korean_menu.py
@@ -22,7 +22,9 @@ def make_sidebar():
 
         if st.session_state.get("logged_in", True):
             st.page_link("pages/korean_chatbot.py", label="당신의 카운셀러", icon="🩹")
+            st.title('')
             st.page_link("pages/korean_analysis.py", label="내 정보", icon="ℹ️")
+            st.title('')
             st.page_link("pages/korean_instruction.py", label="사용법", icon="❓")
 
             st.write("")
