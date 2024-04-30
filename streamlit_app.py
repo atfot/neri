96a939e428d7d9f366044985ea253596fe471206
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 15:27:44 +0900

    qwer

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if 'messages' not in st.session_state:
         language_selection=st.toggle('**한국어/English**', value=False if st.session_state.korean_mode==1 else True)
 
     if language_selection: 
-        st.session_state.korean_mode=1
+        #st.session_state.korean_mode=1
         st.markdown('<p><b>Korean Language Mode</b></p>', unsafe_allow_html=True)
         st.markdown('<center><h1>ᆞ네ᆞᆞ리ᆞ</h1></center>', unsafe_allow_html=True)
         st.markdown('<div style="text-align: right;"><p>로그인 해주세요</p></div>',unsafe_allow_html=True)
