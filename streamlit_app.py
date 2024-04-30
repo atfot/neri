2001ac55e38dfd00834312c6d317d22e7fa2c593
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 22:34:33 +0900

    ㅎㅈㄷㅁ

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,32 +22,32 @@ if 'messages' not in st.session_state:
 }
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div:nth-child(1) > div > div > div > div > div > button > div > p {
 	font-family: 'Beeunhye';
-    font-size: 1.5em;
+    font-size: 1.75em;
 }             
                 
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div:nth-child(2) > div > div > div > div > div > button > div > p {
 	font-family: 'Beeunhye';
-    font-size: 1.5em;
+    font-size: 1.75em;
 }             
 
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div:nth-child(6) > div > button > div > p > strong {
 	font-family: 'Beeunhye';
-    font-size: 1.5em;
+    font-size: 1.75em;
 }             
 
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div:nth-child(7) > div > button > div > p > strong {
 	font-family: 'Beeunhye';
-    font-size: 1.5em;
+    font-size: 1.75em;
 }        
 
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div:nth-child(8) > div > button > div > p {
 	font-family: 'Beeunhye';
-    font-size: 1.5em;
+    font-size: 1.75em;
 }     
                 
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div:nth-child(9) > div > button > div > p {
 	font-family: 'Beeunhye';
-    font-size: 1.5em;
+    font-size: 1.75em;
 }     
                 
 #ce27b8f > div > span > center {
