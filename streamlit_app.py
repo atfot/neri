8167db5bc4e40d88e5d4790c280c64dd4a2c4b26
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 15:31:00 +0900

    qrwe

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,12 +38,11 @@ if 'messages' not in st.session_state:
         if not language_selection: 
             st.markdown('<center><h1>ᆞ네ᆞᆞ리ᆞ</h1></center>', unsafe_allow_html=True)
         if language_selection:
-            st.markdown('<center><h2>ᆞNᆞᆞEᆞᆞRᆞᆞIᆞ</h2></center>', unsafe_allow_html=True)
+            st.markdown('<center><h1>ᆞNᆞᆞEᆞᆞRᆞᆞIᆞ</h1></center>', unsafe_allow_html=True)
     with col2:
         if not language_selection: 
             #st.session_state.korean_mode=1
             st.markdown('<p><b>Korean Language Mode</b></p>', unsafe_allow_html=True)
-            st.markdown('<center><h1>ᆞ네ᆞᆞ리ᆞ</h1></center>', unsafe_allow_html=True)
             st.markdown('<div style="text-align: right;"><p>로그인 해주세요</p></div>',unsafe_allow_html=True)
             
             username = st.text_input("**아이디**")
