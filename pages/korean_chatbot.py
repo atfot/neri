3de482610f72c074b349608369fda08113e43314
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 18:38:39 +0900

    ㅈㄷㄱㄳ

--- pages/korean_chatbot.py
+++ pages/korean_chatbot.py
@@ -217,9 +217,9 @@ def main():
                 "content": f"{user_prompt_1}"
             }
             ],
-            temperature=0.4,
+            temperature=0.5,
             max_tokens=1028,
-            top_p=0.4,
+            top_p=0.5,
             frequency_penalty=1,
             presence_penalty=1
         )
