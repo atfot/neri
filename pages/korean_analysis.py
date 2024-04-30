a2a9bfd82fe67bfbeb5beb49dbfedf204903e6b1
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 22:58:35 +0900

    atew

--- pages/korean_analysis.py
+++ pages/korean_analysis.py
@@ -45,7 +45,7 @@ st.markdown("""
             
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div > div > div > p {
 	font-family: 'Beeunhye';
-	font-size: 1.5em;
+	font-size: 1.75em;
 }     
             
 </style>
