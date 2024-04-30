e0facad673dd027ec85031e5d5a046e6dff8e2c5
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 17:20:08 +0900

    ㅁㅇㄴ

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,16 +31,14 @@ if 'messages' not in st.session_state:
         st.session_state.id = False
         st.session_state.pw = False
 
-    language_selection=st.toggle('**한국어 모드/English Mode**', value=False if st.session_state.korean_mode==1 else True)
+    language_selection=st.toggle('**한국어 버전/English 버전**', value=False if st.session_state.korean_mode==1 else True)
 
     col1,col2=st.columns([7.75,2.25])
     with col1:
         if not language_selection: 
-            st.markdown('<p><b>Korean Language Mode</b></p>', unsafe_allow_html=True)
             st.markdown('<center><h1>ᆞ네ᆞᆞ리ᆞ</h1></center>', unsafe_allow_html=True)
         if language_selection:
             st.session_state.korean_mode=0
-            st.markdown('<p><b>영어 모드</b></p>', unsafe_allow_html=True)
             st.markdown('<center><h1>ᆞNᆞᆞEᆞᆞRᆞᆞIᆞ</h1></center>', unsafe_allow_html=True)
     with col2:
         if not language_selection: 
