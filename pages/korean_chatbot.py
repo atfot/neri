ee6c7f192716e0e6b7fefb2294e6173cce205c74
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 18:37:29 +0900

    ㅈㅅㄷㅁ

--- pages/korean_chatbot.py
+++ pages/korean_chatbot.py
@@ -217,9 +217,9 @@ def main():
                 "content": f"{user_prompt_1}"
             }
             ],
-            temperature=0.1,
+            temperature=0.2,
             max_tokens=1028,
-            top_p=0.1,
+            top_p=0.2,
             frequency_penalty=1,
             presence_penalty=1
         )
