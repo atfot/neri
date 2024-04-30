bc59d39a397f02effe5ce8959d0d9769943c7879
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 16:11:53 +0900

    ㅄㅈㄷ

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,8 @@ if 'messages' not in st.session_state:
             st.title('')
             st.markdown('<div style="text-align: right;"><p><h6>로그인 해주세요</h6></p></div>',unsafe_allow_html=True)
             
-            username = st.text_input("**아이디**")
+            st.markdown('<p><h6>아이디</h6></p>',unsafe_allow_html=True)
+            username = st.text_input("")
             password = st.text_input("**비밀번호**", type="password")
 
             if st.session_state.screen_setting=='pc':
