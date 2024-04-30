84f05073a442c7ad81e8d5ecdb76da63e8f3dbb8
atfot <104991246+atfot@users.noreply.github.com>
2024-04-30 22:45:03 +0900

    rwa

--- korean_menu.py
+++ korean_menu.py
@@ -39,7 +39,7 @@ def make_sidebar():
 }       
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.st-emotion-cache-ugcgyn.eczjsme11 > div.st-emotion-cache-6qob1r.eczjsme3 > div.st-emotion-cache-16txtl3.eczjsme4 > div > div > div > div > div:nth-child(7) > div > div > a > span.st-emotion-cache-ejysk0.e11k5jya0 > div > p {
 	font-family: 'Beeunhye';
-	font-size: 1.5em;
+	font-size: 1.25em;
 }       
 }       
 </style>
