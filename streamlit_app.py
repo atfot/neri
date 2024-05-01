e0e93a3cf593894d94bee17bf7ecf118747e7eae
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 18:14:29 +0900

    asf

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,6 @@ if 'messages' not in st.session_state:
         if language_selection:
             st.session_state.korean_mode=0
             st.image(['https://images.unsplash.com/photo-1714517615056-e8d12c09b3bd?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D','https://images.unsplash.com/photo-1714402002623-86d68590c545?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'],use_column_width=True)
-            toggle_boolean=''
     with col2:
         st.title('')
         st.title('')
