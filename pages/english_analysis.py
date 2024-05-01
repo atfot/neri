db3ec86eb27200fa516b33c4f56f2476a7b24b5c
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 13:40:11 +0900

    sfa

--- pages/english_analysis.py
+++ pages/english_analysis.py
@@ -47,6 +47,11 @@ st.markdown("""
 	font-family: 'Beeunhye';
 	font-size: 1.75em;
 }     
+
+div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div:nth-child(5) > div > div > ul > li {
+	font-family: 'Beeunhye';
+	font-size: 1.75em;
+}     
             
 </style>
 """, unsafe_allow_html=True)
