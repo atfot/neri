87dbb2fc1897a5b62c0b980877635d768df7fc8e
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 22:57:17 +0900

    ge

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ if 'messages' not in st.session_state:
             toggle_boolean=True
 
     if (x := streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)) is not None:
+        st.write(x)
         if x <= 1440:
             st.session_state.screen_setting = 'compact' 
         else: 
