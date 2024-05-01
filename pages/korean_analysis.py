be778e1642f8a70fa5d31026bce643cc9b0068cf
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 15:13:24 +0900

    ㅎㄷㅈ

--- pages/korean_analysis.py
+++ pages/korean_analysis.py
@@ -11,7 +11,21 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 make_sidebar()
-
+st.markdown("""
+<style>
+            @font-face {
+    font-family: 'Beeunhye';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/naverfont_01@1.0/Beeunhye.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+            /* 정보를 바꿔주세요 */
+            div.st-emotion-cache-keje6w.e1f1d6gn3 > div > div > div > div > div > div > div > div:nth-child(1) > div > div > p {
+            font-family: 'Beeunhye';
+            font-size: 2em;
+            }
+</style>
+""",unsafe_allow_html=True)
 
 def fix_info():
   st.session_state.fix_info=True
