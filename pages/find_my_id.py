d6f7da22589ca9e5ec8f0aece4363611263aff00
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 13:24:47 +0900

    fs

--- pages/find_my_id.py
+++ pages/find_my_id.py
@@ -101,6 +101,10 @@ div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth
 	font-family: 'Beeunhye';
     font-size: 2.25em;
 }
+div.st-emotion-cache-1sdqqxz.e1f1d6gn3 > div > div > div > div:nth-child(3) > div > div > div > div > div > div > p {
+	font-family: 'Beeunhye';
+    font-size: 2em;
+}
             </style>
 """, unsafe_allow_html=True)
 
