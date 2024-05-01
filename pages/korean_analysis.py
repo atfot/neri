3d77aeab66448075b78418d0c1a83c23e9266438
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 14:49:33 +0900

    ㄴㄹ

--- pages/korean_analysis.py
+++ pages/korean_analysis.py
@@ -31,12 +31,14 @@ div.st-emotion-cache-1yycg8b.e1f1d6gn3 > div > div > div > div > div > div > p{
 	font-family: 'Beeunhye';
 	font-size: 1.75em;
 }     
-            
-   
-#root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div.st-emotion-cache-1yycg8b.e1f1d6gn3 > div > div > div > div:nth-child(4) > div > button > div > p {
+
+/* 분석결과 내용 */            
+div.st-emotion-cache-keje6w.e1f1d6gn3 > div > div > div > div > div > div > p {
 	font-family: 'Beeunhye';
 	font-size: 1.75em;
 }     
+            
+
             
 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-uf99v8.ea3mdgi8 > div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div:nth-child(4) > div > div > p > b {
 	font-family: 'Beeunhye';
