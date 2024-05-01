8448e8fa8e74081658c42be42674fc0e336cd0e8
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 23:41:32 +0900

    fas

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,6 @@ if 'messages' not in st.session_state:
         col1,col2=st.columns([7.75,2.25])
         with col1:
             st.title('')
-            st.title('')
             if not language_selection: 
                 st.session_state.korean_mode=1
                 st.image(['https://images.unsplash.com/photo-1714362444974-72dedd0d3bfa?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'],use_column_width=True)
