63be1c92bf85d8a6b8259f10648ec7009bea5ae1
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 13:04:32 +0900

    sf

--- pages/find_my_id.py
+++ pages/find_my_id.py
@@ -86,6 +86,11 @@ div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth
 	font-family: 'Beeunhye';
     font-size: 2em;
 }
+            
+div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth-child(10) > div > div > div > div > div > div > p {
+	font-family: 'Beeunhye';
+    font-size: 2em;
+}
             </style>
 """, unsafe_allow_html=True)
 if 'id' not in st.session_state:
