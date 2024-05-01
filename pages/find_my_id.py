4aca8681817620156a06f7bc9a20ed2bac299e71
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 13:00:19 +0900

    ge

--- pages/find_my_id.py
+++ pages/find_my_id.py
@@ -59,7 +59,7 @@ div.st-emotion-cache-zq5wmm.ezrtsby0 > div > div:nth-child(1) > button > div > s
             
 div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth-child(6) > div > div > div > div > div > div > p {
 	font-family: 'Beeunhye';
-    font-size: 2em;
+    font-size: 1.75em;
 }
 
             </style>
