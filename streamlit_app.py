9df11464f2b4accd70419fe72ea45cfed34a4ef1
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 22:38:30 +0900

    gwea

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,6 @@ if 'messages' not in st.session_state:
             toggle_boolean=True
 
     if (x := streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)) is not None:
-        st.write(x)
         st.session_state.screen_setting = 'compact' if x <= 1440 else 'full'
 
     if 'many_login_attempt' not in  st.session_state:
