5dafba5a3382785cf5114a7e1e1ea31493e72a7a
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 14:30:45 +0900

    ㅁㄴㄹ

--- pages/find_my_id.py
+++ pages/find_my_id.py
@@ -63,15 +63,7 @@ div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth
 	font-family: 'Beeunhye';
     font-size: 2em;
 }
-            
-div.block-container.st-emotion-cache-gh2jqd.ea3mdgi5 > div > div > div > div:nth-child(9) > div > label > div > p {
-	font-family: 'Beeunhye';
-    font-size: 2em;
-}
-.st-emotion-cache-10trblm {
-	font-family: 'Beeunhye';
-    font-size: 2.25em;
-}
+
             
 /* 성공 메세지 */
 div.st-emotion-cache-1sdqqxz.e1f1d6gn3 > div > div > div > div:nth-child(3) > div > div > div > div > div > div > p {
