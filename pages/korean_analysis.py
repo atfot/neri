6b37185208bab5b7b9ffc88e8186c05af89388e7
atfot <104991246+atfot@users.noreply.github.com>
2024-05-01 14:58:44 +0900

    ㄹㄴㅁ

--- pages/korean_analysis.py
+++ pages/korean_analysis.py
@@ -46,13 +46,13 @@ div.st-emotion-cache-1yycg8b.e1f1d6gn3 > div > div > div > div > div > button > 
 }     
 
 /* 도움이 될만한 행동들 */         
-
+div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div > div > div > p {
+	font-family: 'Beeunhye';
+	font-size: 1.75em;
+}     
             
 /* 도움이 될만한 행동들 리스트 */  
-div.block-container.st-emotion-cache-z5fcl4.ea3mdgi5 > div > div > div > div > div > div > ul > li {
-	font-family: 'Beeunhye';
-	font-size: 2em;
-}
+
 </style>
 """, unsafe_allow_html=True)
 
