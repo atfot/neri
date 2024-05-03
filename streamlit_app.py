0864fce6711136f9bb13033fb87cccec0e5802e9
atfot <104991246+atfot@users.noreply.github.com>
2024-05-03 22:54:10 +0900

    few

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,7 @@ div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > 
 div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > div > button > div > p {
 	font-family: 'Beeunhye';
     font-size: 1.75em;
+    color: #7F3300;
 }                
                 
 /* 아이디 비번 찾기 새로 오신 분 */
