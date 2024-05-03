483ebf06837b1550b785ba0468953743740c737c
atfot <104991246+atfot@users.noreply.github.com>
2024-05-03 18:10:22 +0900

    FAS

--- streamlit_app.py
+++ streamlit_app.py
@@ -220,7 +220,7 @@ if 'messages' not in st.session_state:
 
     st.markdown(
         """
-        <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #00CED4; color: #FFFFFF; padding: 1px; text-align: center;">
+        <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #562400; color: #FFFFFF; padding: 1px; text-align: center;">
             Developed By <a  href="https://drive.google.com/file/d/1l7duTvc4pWDJgZzY301wswYoIrfylC1G/view?usp=sharing" target="_blank">Hyun Kyu Cho</a> | Made with Streamlit | Powered By OpenAI
         </div>
         """,
