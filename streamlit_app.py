e327752a64d7a6ff8843e45be295aa47d645d2d3
atfot <104991246+atfot@users.noreply.github.com>
2024-05-03 20:04:37 +0900

    zdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,10 @@ div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > 
 	font-family: 'Beeunhye';
     font-size: 1.75em;
 }
+div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div > div > div > div > div > div > button > div > p {
+	font-family: 'Beeunhye';
+    font-size: 1.75em;
+}
 
 /* 실패 메세지 */
 div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > div > div > div > div > div > div > p {
