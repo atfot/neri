67eacd06eb28e26516a42da60bd89fb9b0dde156
atfot <104991246+atfot@users.noreply.github.com>
2024-05-03 19:27:01 +0900

    asf

--- streamlit_app.py
+++ streamlit_app.py
@@ -236,9 +236,7 @@ if 'messages' not in st.session_state:
         st.markdown(
             """
             <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #FDF6EC; color: #000000; padding: 1px; text-align: left;">
-                - Developed By <a  href="https://drive.google.com/file/d/1l7duTvc4pWDJgZzY301wswYoIrfylC1G/view?usp=sharing" target="_blank">Hyun Kyu Cho</a>                 
-                - Made with Streamlit                  
-                - Powered By OpenAI
+                Developed By <a  href="https://drive.google.com/file/d/1l7duTvc4pWDJgZzY301wswYoIrfylC1G/view?usp=sharing" target="_blank">Hyun Kyu Cho</a> | Made with Streamlit | Powered By OpenAI
             </div>
             """,
             unsafe_allow_html=True
