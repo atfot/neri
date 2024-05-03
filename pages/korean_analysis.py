1a4257024ccc4c3165dcd0c6f3a2a85808139f65
atfot <104991246+atfot@users.noreply.github.com>
2024-05-03 21:57:27 +0900

    ge

--- pages/korean_analysis.py
+++ pages/korean_analysis.py
@@ -189,7 +189,7 @@ with col3:
       if st.form_submit_button('**완료**'):
         if x==4:
           st.write('**저장 완료되었습니다👍**')
-          time.sleep(60)
+          time.sleep(1)
           del st.session_state.my_info
           st.rerun()
         else:
