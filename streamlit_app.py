02bb5c41f9671ec89188eafa0b0d7795f2f99b35
atfot <104991246+atfot@users.noreply.github.com>
2024-05-03 19:58:42 +0900

    zfe

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,12 +121,13 @@ div.st-emotion-cache-19or5k2.en6cib61.StatusWidget-enter-done > div > span > but
     language_selection=st.toggle('**한국어 버전 / English Version**', value=toggle_boolean)
     if st.session_state.screen_setting=='full':
         st.title('')
+        st.write('')
     else:
-        pass
+        st.write('')
     if not language_selection: 
         st.session_state.korean_mode=1
         st.image(["https://i.imgur.com/oUkIzkS.png"],use_column_width=True)
-        
+        st.write('')
         st.markdown('<div style="text-align: right;"><p><b>로그인 해주세요</b></p></div>',unsafe_allow_html=True)        
         username = st.text_input("**아이디**") 
         password = st.text_input("**비밀번호**", type="password")
