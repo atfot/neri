934bbfd5765a9f3b4aaeed9cac40ffeceb87742f
atfot <104991246+atfot@users.noreply.github.com>
2024-05-05 14:56:49 +0900

    sda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from time import sleep
 from streamlit_js_eval import streamlit_js_eval
-from PIL import ImageGrab
+import pyautogui
 
 if 'messages' not in st.session_state:
     if 'screen_setting' not in st.session_state:
@@ -25,8 +25,8 @@ if 'messages' not in st.session_state:
 
     if (x := streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)) is not None:
         st.write(x)
-        st.session_state.screen_setting = 'compact' if x < 1100 else 'full'
-    
+        st.session_state.screen_setting = 'compact' if x < 1100 else 'full'    
+    st.write(pyautogui.size())
 
     if 'many_login_attempt' not in  st.session_state:
         st.session_state.many_login_attempt=False
