d687155902cd6a264c3c9dafabf7d862422360bf
atfot <104991246+atfot@users.noreply.github.com>
2024-05-05 21:34:54 +0900

    sa

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ div.st-bl.st-bm.st-bn.st-bo.st-bp.st-bq.st-br.st-bs.st-bt.st-bu.st-bv > div > di
 div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div > div > div > div > p > b {
 	font-family: 'Beeunhye';
     font-size: 1.75em;
-    letter-spacing:0.5em;
+    letter-spacing:0.25em;
 }
                 
 /* 아이디_비밀번호 */
