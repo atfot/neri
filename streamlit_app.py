5a71a1dc2d9506726d1a17490b8145ae3e9cf312
atfot <104991246+atfot@users.noreply.github.com>
2024-05-05 14:48:54 +0900

    fas

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ if 'messages' not in st.session_state:
     if (x := streamlit_js_eval(js_expressions='window.innerWidth', key='WIDTH', want_output=True)) is not None:
         st.write(x)
         st.session_state.screen_setting = 'compact' if x < 1100 else 'full'
-    img = ImageGrab.grab()
-    print (img.size)
+
 
     if 'many_login_attempt' not in  st.session_state:
         st.session_state.many_login_attempt=False
