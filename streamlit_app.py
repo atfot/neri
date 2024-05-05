10c4134e62ae3b4a4e534ebf8be96afa339da1b5
atfot <104991246+atfot@users.noreply.github.com>
2024-05-05 21:49:11 +0900

    ㅁㄴㅇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,6 @@ div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div > div > label > d
 div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div > div > button > div > p {
 	font-family: 'Beeunhye';
     font-size: 1.75em;
-    color: #7F3300;
     letter-spacing:0.1em;
 }                
                 
@@ -63,7 +62,6 @@ div.st-emotion-cache-1bfnhmd.e1f1d6gn3 > div > div > div > div > div > button > 
 div.st-emotion-cache-ocqkz7.e1f1d6gn5 > div > div > div > div > div > div > button > div > p {
 	font-family: 'Beeunhye';
     font-size: 1.75em;
-    color: #89482C;
     letter-spacing:0.1em;
 }
 
