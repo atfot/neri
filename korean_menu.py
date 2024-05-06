896ee1d1049853d67f008016b10da21d611a4df5
atfot <104991246+atfot@users.noreply.github.com>
2024-05-06 17:37:45 +0900

    sadf

--- korean_menu.py
+++ korean_menu.py
@@ -50,7 +50,7 @@ div.st-emotion-cache-19or5k2.en6cib61.StatusWidget-enter-done > div > span > but
 /* 메뉴 */
 span.st-emotion-cache-icvz16.e11k5jya0 > div > p {
 	font-family: 'Beeunhye';
-	font-size: 2em;
+	font-size: 1.75em;
     margin-top: -0.1em; 
     margin-left: 0.25em; 
     letter-spacing:0.075em;
