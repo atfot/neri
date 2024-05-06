a77dfeeb702d87796c06e7d43ba3759f4d39cc43
atfot <104991246+atfot@users.noreply.github.com>
2024-05-06 18:07:25 +0900

    fdsz

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,7 +272,7 @@ div.st-emotion-cache-19or5k2.en6cib61.StatusWidget-enter-done > div > span > but
         col1,col2=st.columns([7.75,2.25])
         with col1:
             if st.session_state.screen_setting=='pc':
-                st.image(["https://imgur.com/MvfBBoV.png"],
+                st.image(["https://imgur.com/Ye5F8qs.png"],
         use_column_width=True)
             if st.session_state.screen_setting=='tablet':
                 st.image(["https://imgur.com/chYXJ98.png"],
