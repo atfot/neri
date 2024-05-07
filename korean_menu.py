fe5784bdf442a030c0852320980d2e42d4234d66
atfot <104991246+atfot@users.noreply.github.com>
2024-05-07 17:17:51 +0900

    ㄴㅁㄹ

--- korean_menu.py
+++ korean_menu.py
@@ -163,7 +163,15 @@ div.st-emotion-cache-keje6w.e1f1d6gn3 > div > div > div > div > div > div > div 
 }
                     
                     /* 오류 보고 */
+                    /* 안내문*/
                     div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > div > div > p {
+    font-family: 'Beeunhye';
+    font-size: 1.75em;
+    letter-spacing:0.075em;
+}
+                    
+                    /* 작성란 */
+                    div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > div > label > div > p {
     font-family: 'Beeunhye';
     font-size: 1.75em;
     letter-spacing:0.075em;
