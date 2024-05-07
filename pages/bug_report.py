f4bdde1e03fe5a8963b30844afcd242ab0fc5f28
atfot <104991246+atfot@users.noreply.github.com>
2024-05-07 17:03:08 +0900

    etw

--- pages/bug_report.py
+++ pages/bug_report.py
@@ -26,9 +26,9 @@ st.write("""
 
 # Taking inputs
 
-error_subject = st.text_input('제목')
-error_body = st.text_area('내용')
-error_images=st.file_uploader('상세사진',accept_multiple_files=True)
+error_subject = st.text_input('어떤 오류가 나셨나요?')
+error_body = st.text_area('해당 오류에 대한 자세한 설명을 해주세요!')
+error_images=st.file_uploader('실제로',accept_multiple_files=True)
 
 col1,col2=st.columns([8,2])
 with col2:
@@ -55,10 +55,11 @@ if st.session_state.send_email==True:
         msg.attach(content)
         
         # 이미지 파일 추가
-        for image in error_images:
-            img = MIMEImage(image.read())
-            img.add_header('Content-Disposition', 'attachment', filename=image.name)
-            msg.attach(img)
+        if error_images is not None:
+            for image in error_images:
+                img = MIMEImage(image.read())
+                img.add_header('Content-Disposition', 'attachment', filename=image.name)
+                msg.attach(img)
         
         # 받는 메일 유효성 검사 거친 후 메일 전송
         smtp.sendmail(st.secrets.admin_email, st.secrets.bug_report_email, msg.as_string())
