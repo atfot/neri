fbbd7a3d94c4523cac5a6002af3b6e79ec59128b
atfot <104991246+atfot@users.noreply.github.com>
2024-05-08 20:50:58 +0900

    asf

--- pages/korean_analysis.py
+++ pages/korean_analysis.py
@@ -223,12 +223,12 @@ if sss.fix_info==False:
   with col2:
     if st.button('고객님의 정보를 이메일로 받아보시겠어요?',key='send_userinfo',use_container_width=True):
       html_text_1="""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Neri의 상담분석</title>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Neri의 상담분석</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 """
       html_problem=sss.problem_explanation.replace('\n', '<br>')
@@ -237,34 +237,75 @@ if sss.fix_info==False:
       html_client_analysis=sss.client_analysis.replace('\n', '<br>')
       html_score_explanation=sss.score_explanation.replace('\n', '<br>')
       html_text_2=f"""
-<body style="font-size: 16pt; background-color: #fff; color: #000; margin: 0; padding: 1em; justify-content: space-around;">
-    <header style="width:100; text-align: center;">
-        <h1>{sss.username}님의 분석 결과</h1>
-    </header>
-    <div class="analysis" style="width: 100%; overflow: auto;">
-      <div class="half" style="width: 50%; float: left; padding:2em; box-sizing: border-box;">
-          <h2>고객님의 정보</h2>
-          <p><b>1. 고객님 성함 : </b>{sss.username}</p>
-          <p><b>2. 연령 : </b>{sss.age}</p>
-          <p><b>3. 성별 : </b>{sss.gender}</p>
-          <p><b>4. 고민 : </b>{html_problem}</p>
-          <p><b>5. 고민 설명 : </b></p>
-          <p>{html_problem_explanation}</p>
-          <p><b>6. 목표 : </b></p>
-          <p>{html_goal}</p>
-      </div>
-
-      <div class="half" style="width: 50%; float: left; padding:2em; box-sizing: border-box;">
-          <h2>{sss.date}의 분석 결과</h2>
-          <p><b>문제분석 : </b></p>
-          <p>{html_client_analysis}</p>
-          <p><b>해결 진전도 : </b>{sss.score}</p>
-          <p><b>채점 기준 : </b></p>
-          <p>{html_score_explanation}</p>
-      </div>
-  </div>
-  <div class="whattodo" style="border-radius: 5px; align-self: center; width: 50%; margin: 0 auto; border: 0.01em solid black; padding: 2em;">
-      <p><h3>도움이 될만한 행동들 : </h3></p>
+<body style="margin: 0; padding: 20px 0 30px 0;">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="800" style="border: 1px solid #cccccc;">
+        <tr>
+            <td align="center">
+                <h1>{sss.date}의 분석 결과</h1>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td>
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                <tr>
+                                    <td align="right" width="720" valign="top">
+                                        Powered by
+                                    </td>
+                                    <td align="right" width="140" valign="top">
+                                        <img src="https://imgur.com/H287o5n.png" alt="Logo" width="140" height="60" style="display: block;" />
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="color: #000000; font-family: Arial, sans-serif; font-size: 24px;">
+                            <b>인공지능 네리가 분석한 보고서입니다!</b>
+                        </td>
+                    </tr> 
+                    <tr style="color: #000000; font-family: Arial, sans-serif; font-size: 16px; line-height: 30px;"
+                    >
+                        <td style="padding: 20px 0 30px 0;">
+                            네리는 OpenAI사의 GPT 3.5와 4.0을 사용해 제작된 인공지능 심리상담 챗봇입니다. 이 보고서는 금일 해당 사용자가 네리와 대화하며 도출된 분석결과입니다. 참고만 하시길 바라며 자세한건 가까운 정신과나 심리상담사를 직접 방문하셔서 도움을 받으세요.
+                        </td>
+                    </tr>
+                    <tr style="color: #000000; font-family: Arial, sans-serif; font-size: 16px; line-height: 30px;"
+                    >
+                        <td>
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                <tr>
+                                    <td width="260" valign="top">
+                                        <h2>고객님의 정보</h2>
+                                        <p><b>1. 고객님 성함 : </b>{sss.username}</p>
+                                        <p><b>2. 연령 : </b>{sss.age}</p>
+                                        <p><b>3. 성별 : </b>{sss.gender}</p>
+                                        <p><b>4. 고민 : </b>{html_problem}</p>
+                                        <p><b>5. 고민 설명 : </b></p>
+                                        <p>{html_problem_explanation}</p>
+                                        <p><b>6. 목표 : </b></p>
+                                        <p>{html_goal}</p>
+                                    </td>
+                                    <td style="font-size: 0; line-height: 0;" width="20">
+                                    &nbsp;
+                                    </td>
+                                    <td width="260" valign="top">
+                                        <h2>{sss.username}님의 분석 결과</h2>
+                                        <p><b>문제분석 : </b></p>
+                                        <p>{html_client_analysis}</p>
+                                        <p><b>해결 진전도 : </b>{sss.score}</p>
+                                        <p><b>채점 기준 : </b></p>
+                                        <p>{html_score_explanation}</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="color: #000000; font-family: Arial, sans-serif; font-size: 16px; line-height: 30px; border-radius: 5px; align-self: center; width: 100%; margin: 0 auto; border: 0.1px solid #cccccc; padding: 20px;">
+                            <p><b>도움이 될만한 행동들 : </b></p>
 """
       todolist_format="""
         <ul>
@@ -277,14 +318,34 @@ if sss.fix_info==False:
       for i in sss.what_to_do:
         html_text_3+=todolist_format.format(i)
       html_text_4="""
-    </div>
-    <div class="graph" style="text-align: center;">
-        <h2>그래프</h2>
-    </div>
-    <footer style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #Fff; color: #000000; padding: 1px; text-align: center;">
-        Developed By <a  href="https://drive.google.com/file/d/1l7duTvc4pWDJgZzY301wswYoIrfylC1G/view?usp=sharing" target="_blank">Hyun Kyu Cho</a>  |  Made with Streamlit  |  Powered By OpenAI
-    </footer>
-</body>
+                        </td>
+                    </tr>
+                    <tr align="center" width="100%" style="color: #000000; font-family: Arial, sans-serif; font-size: 16px; line-height: 30px;">
+                        <td>
+                            <h2>그래프</h2>
+                        </td>
+                    </tr>
+                    <tr align="center" width="100%">
+                        <td>
+                            <img src="https://imgur.com/MvfBBoV.png" width="720" height="280" style="display: block;" />
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td style="padding: 30px 30px 30px 30px; bottom: 0; left: 0; width: 100%; background-color: #FFFFFF; color: #000000; padding: 1px; text-align: center;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td>
+                            Developed By <a href="https://drive.google.com/file/d/1l7duTvc4pWDJgZzY301wswYoIrfylC1G/view?usp=sharing" color="#000000" target="_blank"><font color="#000000">Hyun Kyu Cho</font></a> |  Made with Streamlit  |  Powered By OpenAI
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+       </table>
+   </body>
 </html>"""
       html=html_text_1+html_text_2+html_text_3+html_text_4
       try:
