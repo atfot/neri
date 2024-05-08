532169f7f310947d6800dff29efae2f231e8b03e
atfot <104991246+atfot@users.noreply.github.com>
2024-05-08 22:12:07 +0900

    fas

--- pages/korean_bug_report.py
+++ pages/korean_bug_report.py
@@ -3,7 +3,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart  
 from email.mime.text import MIMEText  
 from email.mime.image import MIMEImage  
-from korean_menu import make_sidebar
+from english_menu import make_sidebar
 
 st.set_page_config(
     page_title="당신의 AI 심리상담사, 네리",
