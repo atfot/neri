19778f292b8b2af998bdb1a3e6439d8b8b163a0d
atfot <104991246+atfot@users.noreply.github.com>
2024-05-09 15:45:59 +0900

    gsad

--- pages/korean_instruction.py
+++ pages/korean_instruction.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from korean_menu import make_sidebar
+from fpdf import FPDF
 
 st.set_page_config(
     page_title="당신의 AI 심리상담사, 네리",
@@ -9,4 +10,9 @@ st.set_page_config(
 make_sidebar()
 
 if st.button('try'):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, 'Hello World!')
+    pdf.output('tuto1.pdf', 'F')
     st.write('success')
