137d0bbfb2c7ce3437ed91cb71c989e5f39a3052
atfot <104991246+atfot@users.noreply.github.com>
2024-05-09 15:00:19 +0900

    asfd

--- pages/korean_instruction.py
+++ pages/korean_instruction.py
@@ -8,14 +8,5 @@ st.set_page_config(
 )
 make_sidebar()
 
-st.title('네리를 만나러 오신 여러분을 환영합니다!')
-st.title('')
-st.write("""
-네리는 **OpenAI사의 ChatGPT API**를 사용하여 만들어진 **인공지능 챗봇**이에요.
-         
-현규 : 솔직히 여기서 뭘 말해야할지 좀 고민해봐야겠다..ㅅㅂ...
-
-
-         
-
-""")
+if st.button('try'):
+    st.write('success')
