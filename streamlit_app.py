c47dcd8b16d8d4fa44d87b1f9beefea1851b00e9
atfot <104991246+atfot@users.noreply.github.com>
2024-05-14 21:06:20 +0900

    dsa

--- streamlit_app.py
+++ streamlit_app.py
@@ -388,7 +388,7 @@ div.st-emotion-cache-19or5k2.en6cib61.StatusWidget-enter-done > div > span > but
     if sss.screen_setting=='mobile':
         st.markdown(
             """
-            <div style="bottom: 0; left: 0; width: 101%; background-color: #FDF6EC; color: #000000; text-align: left;">
+            <div style="bottom: 0; left: 0; width: 101%; background-color: #ffffff; color: #000000; text-align: left;">
                 Developed By <a  href="https://i.imgur.com/JuFxv4h.png" target="_blank">Hyun Kyu Cho</a><br>
                 Made with Streamlit<br>
                 Powered By OpenAI
