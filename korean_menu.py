1dd2fef6c21c4eb05ff7b84b22cecdc6802b1ec7
atfot <104991246+atfot@users.noreply.github.com>
2024-05-14 20:45:13 +0900

    ㅁㄷㅈㄹ

--- korean_menu.py
+++ korean_menu.py
@@ -97,6 +97,12 @@ div.st-emotion-cache-c6gdys.e18r7x300 > div > p {
 /* 기본 메세지 */
 div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > div > div > p {
 font-family: 'Beeunhye';
+font-size: 1.75em;
+letter-spacing:0.075em;
+}
+/* 설명란 */                    
+div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > div > label > div > p {
+font-family: 'Beeunhye';
 font-size: 2em;
 letter-spacing:0.075em;
 }
