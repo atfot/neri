bfa63ec9bf757ab0dbc2029ad07b4055aaddd237
atfot <104991246+atfot@users.noreply.github.com>
2024-05-14 11:35:13 +0900

    ewg

--- pages/korean_instruction_2.py
+++ pages/korean_instruction_2.py
@@ -170,7 +170,7 @@ html_content_1=f'''<body style="margin: 0; padding: 20px 0 30px 0;">
                             네리는 OpenAI사의 GPT 3.5와 4.0을 사용해 제작된 인공지능 심리상담 챗봇입니다. 이 보고서는 금일 해당 사용자가 네리와 대화하며 도출된 분석결과입니다. 참고만 하시길 바라며 자세한건 가까운 정신과나 심리상담사를 직접 방문하셔서 도움을 받으세요.
                         </td>
                     </tr>
-                    <tr style="color: #000000; font-size: 16px; line-height: 50px;"
+                    <tr style="color: #000000; font-size: 16px; line-height: 25px;"
                     >
                         <td>
                             <table border="0" cellpadding="0" cellspacing="0" width="100%">
@@ -202,7 +202,7 @@ html_content_1=f'''<body style="margin: 0; padding: 20px 0 30px 0;">
                         </td>
                     </tr>
                     <tr>
-                        <td style="color: #000000; font-size: 16px; line-height: 30px;">
+                        <td style="color: #000000; font-size: 16px; line-height: 15px;">
                             <p><br></p>
                         </td>
                     </tr>
