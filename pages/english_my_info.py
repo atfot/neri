84be42bf769e49c443866911c50cde8e81e3ebd6
atfot <104991246+atfot@users.noreply.github.com>
2024-05-14 19:15:33 +0900

    eawg

--- pages/english_my_info.py
+++ pages/english_my_info.py
@@ -35,97 +35,94 @@ if 'fix_unfinished' not in sss:
 def fix_info():
   sss.fix_info=True
 
-col1,col2=st.columns([9,1])
+col1,col2=st.columns([5,5])
 with col1:
-    if sss.fix_info==False:
-        st.write('')
-        st.markdown('<h4>My Profile</h4>',unsafe_allow_html=True)
-        st.markdown(f'''
-                    <p>
-                    <b>1. Username : </b>{sss.username}
+    st.write('')
+    st.markdown('<h4>My Profile</h4>',unsafe_allow_html=True)
+    st.markdown(f'''
+                <p>
+                <b>1. Username : </b>{sss.username}
 
-                    <b>2. Age : </b>{sss.age}
+                <b>2. Age : </b>{sss.age}
 
-                    <b>3. Gender : </b>{sss.gender}
+                <b>3. Gender : </b>{sss.gender}
 
-                    <b>4. Problem : </b>
-                    
-                    {sss.problem}
+                <b>4. Problem : </b>
+                
+                {sss.problem}
 
-                    <b>5. Problem Explanation : </b>
-                    
-                    {sss.problem_explanation}
+                <b>5. Problem Explanation : </b>
+                
+                {sss.problem_explanation}
 
-                    <b>6. Goal : </b>
-                    
-                    {sss.goal}
-                    </p>
-                    ''', unsafe_allow_html=True)   
-        st.button('Fix my Info',use_container_width=True,on_click=fix_info)  
-    else:
-        sss.filled_input=0
-        st.write("**Now you can fix your info😊**")
+                <b>6. Goal : </b>
+                
+                {sss.goal}
+                </p>
+                ''', unsafe_allow_html=True)   
+    st.button('Fix my Info',use_container_width=True,on_click=fix_info)  
+with col2:        
+    sss.filled_input=0
+    st.write("**Now you can fix your info😊**")
 
-        user_email = st.text_input('**Write down the new email address you want to use.**', key='new_user_email',type='password')
-        if user_email:
-            if not re.match(r'^[\w\.-]+@[\w\.-]+\.[a-zA-Z]+$', user_email): 
-                st.error('Please give the correct email!')
-            else:
-                if user_email != sct.user_email and user_email !=sct.user_email_2 and sss.user_email:
-                    sss.filled_input+=1
-                    #sss.user_email=user_email
-                elif user_email == sss.user_email:
-                    st.error("This email address is the same as the one you're using now.")
-                else:
-                    st.error('This email address already exists.')
-        email_check = st.text_input('**Please write the same email as above again.**',key='email_check',type='password')
-        if email_check:
-            if email_check!=sss.user_email:
-                st.error('That email is different from the one you just wrote down')
-            if email_check==sss.user_email:
-                sss.filled_input+=1
-        username = st.text_input('**Tell me the name you want to be called in here.**',key='new_username')
-        if username:
-            if username!=sct.user_name and username!=sct.user_name_2 and username!=sss.username:
-                sss.filled_input+=1
-                #sss.username=username
-            elif username==sss.username:
-                st.error('This is the same username you were using before.')
-            else:
-                st.error('The username already exists.')
-            
-        problem = st.text_area("**What's your biggest problem right now?🤔**",key='new_problem')
-        if problem:
-            sss.filled_input+=1
-            #sss.problem=problem
-        problem_explanation=st.text_area("**Please describe your issue in more detail. The more details you can provide, the better😊**",key='new_problem_explanation')
-        if problem_explanation:
-            sss.filled_input+=1
-            #sss.problem_explanation=problem_explanation
-        goal=st.text_area("**Tell us what your end goal is!**",key='new_goal')
-        if goal:
-            sss.filled_input+=1
-            #sss.goal=goal
-        col1,col2=st.columns([8,2])
-        with col2:
-            if st.button('**Submit**',use_container_width=True):
-                if sss.filled_input==6:
-                    sss.fix_complete=True
-            else:
-                sss.fix_unfinished==True
-        if sss.fix_complete==True:
-            sss.user_email=user_email
-            sss.username=username
-            sss.problem=problem
-            sss.problem_explanation=problem_explanation
-            sss.goal=goal
-            st.write('**Your user profile is fixed👍**')
-            time.sleep(2)
-            sss.my_info, sss.filled_input, sss.fix_complete=False
-            st.rerun()
-        if sss.fix_unfinished==True:
-            st.write('**Please fill every blanks🙃**')
+    user_email = st.text_input('**Write down the new email address you want to use.**', key='new_user_email',type='password')
+    if user_email:
+        if not re.match(r'^[\w\.-]+@[\w\.-]+\.[a-zA-Z]+$', user_email): 
+            st.error('Please give the correct email!')
         else:
-            pass
-with col2:
-    st.empty()
+            if user_email != sct.user_email and user_email !=sct.user_email_2 and sss.user_email:
+                sss.filled_input+=1
+                #sss.user_email=user_email
+            elif user_email == sss.user_email:
+                st.error("This email address is the same as the one you're using now.")
+            else:
+                st.error('This email address already exists.')
+    email_check = st.text_input('**Please write the same email as above again.**',key='email_check',type='password')
+    if email_check:
+        if email_check!=sss.user_email:
+            st.error('That email is different from the one you just wrote down')
+        if email_check==sss.user_email:
+            sss.filled_input+=1
+    username = st.text_input('**Tell me the name you want to be called in here.**',key='new_username')
+    if username:
+        if username!=sct.user_name and username!=sct.user_name_2 and username!=sss.username:
+            sss.filled_input+=1
+            #sss.username=username
+        elif username==sss.username:
+            st.error('This is the same username you were using before.')
+        else:
+            st.error('The username already exists.')
+        
+    problem = st.text_area("**What's your biggest problem right now?🤔**",key='new_problem')
+    if problem:
+        sss.filled_input+=1
+        #sss.problem=problem
+    problem_explanation=st.text_area("**Please describe your issue in more detail. The more details you can provide, the better😊**",key='new_problem_explanation')
+    if problem_explanation:
+        sss.filled_input+=1
+        #sss.problem_explanation=problem_explanation
+    goal=st.text_area("**Tell us what your end goal is!**",key='new_goal')
+    if goal:
+        sss.filled_input+=1
+        #sss.goal=goal
+    col1,col2=st.columns([8,2])
+    with col2:
+        if st.button('**Submit**',use_container_width=True):
+            if sss.filled_input==6:
+                sss.fix_complete=True
+        else:
+            sss.fix_unfinished==True
+    if sss.fix_complete==True:
+        sss.user_email=user_email
+        sss.username=username
+        sss.problem=problem
+        sss.problem_explanation=problem_explanation
+        sss.goal=goal
+        st.write('**Your user profile is fixed👍**')
+        time.sleep(2)
+        sss.my_info, sss.filled_input, sss.fix_complete=False
+        st.rerun()
+    if sss.fix_unfinished==True:
+        st.write('**Please fill every blanks🙃**')
+    else:
+        pass
