d3276c707af13ef0530d84c58c49e0ff79417df9
atfot <104991246+atfot@users.noreply.github.com>
2024-05-22 21:03:22 +0900

    adsg

--- pages/korean_about_me.py
+++ pages/korean_about_me.py
@@ -2,6 +2,7 @@ import streamlit as st
 from korean_menu import make_sidebar
 from streamlit import session_state as sss
 from security import check
+import socket
 
 st.set_page_config(
     page_title="당신의 AI 심리상담사, 네리",
@@ -15,3 +16,9 @@ except:
 make_sidebar()
 check('hi')
 st.write("개발자의 말 부분임. 나중에 적을 것임")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+local_ip = s.getsockname()[0]
+s.close()
+st.write(f"Your local IP address is: {local_ip}")
