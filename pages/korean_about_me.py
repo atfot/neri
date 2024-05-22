250f385fcd3c64e463adfe16184e62036b54f52d
atfot <104991246+atfot@users.noreply.github.com>
2024-05-22 20:49:06 +0900

    rew

--- pages/korean_about_me.py
+++ pages/korean_about_me.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from korean_menu import make_sidebar
 from streamlit import session_state as sss
+from security import check
 
 st.set_page_config(
     page_title="당신의 AI 심리상담사, 네리",
@@ -12,4 +13,5 @@ try:
 except:
     pass
 make_sidebar()
+check()
 st.write("개발자의 말 부분임. 나중에 적을 것임")
