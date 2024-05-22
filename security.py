97b79684ed5a0c6acd0e2e0e94413c9c89c21bb7
atfot <104991246+atfot@users.noreply.github.com>
2024-05-22 21:13:25 +0900

    gew

--- security.py
+++ security.py
@@ -3,6 +3,10 @@ from streamlit import session_state as sss
 import hashlib
 
 def check(arg):
+    if 'checked_object' not in sss:
+        sss.checked_object=''
     hash_object = hashlib.sha3_512(arg.encode())
     hex_dig = hash_object.hexdigest()
-    st.write(hex_dig)
+    sss.checked_object=hex_dig
+    st.write(sss.checked_object)
+    sss.checked_object=""
