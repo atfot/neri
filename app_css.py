3a7c955a80ae535e768d2cd3613f064aac485610
atfot <104991246+atfot@users.noreply.github.com>
2024-05-30 22:12:32 +0900

    tew

--- app_css.py
+++ app_css.py
@@ -378,7 +378,7 @@ div.st-emotion-cache-15ecox0.ezrtsby0 > div > div > button > div > span {
 }
 
 /* 타이틀 */ 
-div.block-container.st-emotion-cache-13ln4jf.ea3mdgi5 > div > div > div > div > div > div > center > div {
+h3 {
 	font-family: 'Beeunhye';
     font-size: 2.25em;
     letter-spacing:0.075em;
