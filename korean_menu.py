11f450e82dffdfe4ab2eb78d5a2128ddcd76985e
atfot <104991246+atfot@users.noreply.github.com>
2024-06-06 20:35:58 +0900

    asf

--- korean_menu.py
+++ korean_menu.py
@@ -34,9 +34,9 @@ def make_sidebar():
 
             if st.button("로그아웃",type='primary',use_container_width=True):
                 logout()
-            if st.button("내 정보",type='secondary',use_container_width=True):
-                st.switch_page("pages/korean_my_info.py")
             if st.button("대화 저장",type='secondary',use_container_width=True):
+                st.switch_page("pages/korean_my_info.py")
+            if st.button("내 정보",type='secondary',use_container_width=True):
                 st.switch_page("pages/korean_my_info.py")
 
             st.markdown(
