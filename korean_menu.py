2fe4f6b965edf6a8e57e39c308c29918777fc478
atfot <104991246+atfot@users.noreply.github.com>
2024-08-05 14:40:55 +0900

    we

--- korean_menu.py
+++ korean_menu.py
@@ -93,7 +93,7 @@ def logout():
 #            pass
         del sss.client
     st.info("다음에 또 뵈어요😊")
-    sleep(0.5)
+    sleep(30)
     st.switch_page("streamlit_app.py")
 
 def save_analysis_and_messages():    
